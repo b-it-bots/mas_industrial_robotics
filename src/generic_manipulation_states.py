e772a8ef695ae943f5e05a89682f936618c087e8
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-06-20 12:30:50 -0500

    continue work on bmt test

--- src/generic_manipulation_states.py
+++ src/generic_manipulation_states.py
@@ -37,6 +37,18 @@ class grasp_random_object(smach.State):
         return 'failed'
         
 
+class grasp_obj_with_visual_servering(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['object_to_grasp'])
+        
+    def execute(self, userdata):
+        
+        print userdata.object_to_grasp
+        
+        return 'succeeded'
+
+
 class place_obj_on_rear_platform(smach.State):
 
     def __init__(self):
@@ -47,20 +59,39 @@ class place_obj_on_rear_platform(smach.State):
         
         sss.move("arm", "zeroposition")
         sss.move("arm", "pregrasp_back_init")
-        sss.move("arm", "pregrasp_back")
+        #sss.move("arm", "pregrasp_back")
         
         if(len(userdata.rear_platform_free_poses) > 0):
             pltf_pose = userdata.rear_platform_free_poses.pop()
+            sss.move("arm", pltf_pose)
         else:
             pltf_pose = [0.033 + 0.024 - 0.32, 0.0, 0.12, 0, -math.pi + 0.2, 0, "/arm_link_0"];
-        
+            sss.move("arm", [pltf_pose[0], pltf_pose[1], pltf_pose[2], pltf_pose[3], pltf_pose[4], pltf_pose[5], pltf_pose[6]])
        
-        sss.move("arm", [pltf_pose[0], pltf_pose[1], pltf_pose[2], pltf_pose[3], pltf_pose[4], pltf_pose[5], pltf_pose[6]])
+        
         sss.move("gripper", "open")
 
         userdata.rear_platform_occupied_poses.append(pltf_pose)
 
         sss.move("arm", "pregrasp_back")
+        
+        return 'succeeded'
+
+
+class grasp_obj_from_pltf(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'no_more_obj_on_pltf'], 
+                                    input_keys=['rear_platform_occupied_poses'], 
+                                    output_keys=['rear_platform_occupied_poses'])
+        
+    def execute(self, userdata):
+        
+        if(len(rear_platform_occupied_poses) == 0):
+            return 'no_more_obj_on_pltf'
+        
+        
+        print userdata.rear_platform_occupied_poses.pop()
         
         return 'succeeded'
     
@@ -80,3 +111,17 @@ class move_arm_out_of_view(smach.State):
         return 'succeeded'
     
     
+class grasp_obj_from_pltf(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'no_more_obj_on_pltf'], 
+                             input_keys=['rear_platform_occupied_poses'],
+                             output_keys=['rear_platform_occupied_poses'])
+
+    def execute(self, userdata):   
+        
+        print userdata.rear_platform_occupied_poses.pop()
+        
+           
+        return 'succeeded'
+
