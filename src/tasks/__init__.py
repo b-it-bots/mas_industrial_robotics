44c638ead99c24b0201e06680b65fe9769122164
Sergey Alexandrov <alexandrov88@gmail.com>
2013-04-19 19:47:11 +0200

    Added factory constructor to tasks module.

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,3 +1,11 @@
 from bnt_task import BNTTask
 from bmt_task import BMTTask
 from ppt_task import PPTTask
+from task import TaskSpecFormatError
+
+
+def parse_task(task_type, task_spec):
+    TYPE_MAP = {'BNT': BNTTask,
+                'BMT': BMTTask,
+                'PPT': PPTTask}
+    return TYPE_MAP[task_type](task_spec)
