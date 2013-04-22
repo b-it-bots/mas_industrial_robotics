8c87eb2784954d9381bb8a69c90326d549f3f464
Sergey Alexandrov <alexandrov88@gmail.com>
2013-04-22 18:58:50 +0200

    Updated Arm class.

--- include/arm.py
+++ include/arm.py
@@ -7,6 +7,8 @@ roslib.load_manifest(PACKAGE)
 
 import tf
 import rospy
+import numpy as np
+from geometry_msgs.msg import Point, Quaternion
 from actionlib import SimpleActionClient
 from simple_script_server import simple_script_server
 from arm_navigation_msgs.msg import MoveArmAction, MoveArmGoal, \
@@ -16,54 +18,59 @@ from arm_navigation_msgs.msg import MoveArmAction, MoveArmGoal, \
 class Arm(object):
 
     CART_SERVER = '/arm_controller/move_arm_cart'
+    ARM_LINK_5_TO_GRIPPER_OFFSET = np.array([-0.004, -0.002, -0.076, 1])
 
-    def __init__(self, default_pitch=(math.pi / 2.0), planning_mode=''):
+    def __init__(self, planning_mode=''):
         self.move_arm_cart_server = SimpleActionClient(self.CART_SERVER,
                                                        MoveArmAction)
         self.script_server = simple_script_server()
-        self.pitch = default_pitch
         self.planning_mode = planning_mode
         self.blocking = True
 
     def move_to(self, where, blocking=True):
+        """
+        Move the arm to the pose specified in 'where'.
+        Supported formats:
+            * list/tuple with 5 numbers:
+                It is treated as a joint state.
+            * string:
+                It is treated as a pose name (should be on the parameter
+                server).
+            * list/tuple with first element string and 6 numbers:
+                It is treated as a cartesian pose. First item is the frame of
+                reference, the remaining numbers are x, y, z, roll, pitch, yaw.
+        """
         self.blocking = blocking
         if isinstance(where, str):
             self._move_to_pose(where)
-        elif len(where) == 5:
+        elif (len(where) == 5 and
+              all(isinstance(x, (float, int)) for x in where)):
             self._move_to_joints(where)
-        elif len(where) == 4 or len(where) == 3:
+        elif ((len(where) == 7) and
+              isinstance(where[0], str) and
+              all(isinstance(x, (float, int)) for x in where[1:])):
             self._move_to_cartesian(where)
+        else:
+            raise Exception('Unsupported arm target pose format (%s)' % where)
 
     def _move_to_joints(self, joints):
         self.script_server.move('arm', [joints], mode=self.planning_mode,
                                 blocking=self.blocking)
 
     def _move_to_cartesian(self, coordinates):
-        """
-        Move the arm to the pose given by (x, y, z, pitch) tuple. The pitch is
-        optional and can be omitted.
-        """
         rospy.logdebug('Waiting for [%s] server...' % (self.CART_SERVER))
         self.move_arm_cart_server.wait_for_server()
+        position, orientation = self._construct_target_pose(coordinates[1:])
         g = MoveArmGoal()
         pc = PositionConstraint()
-        pc.header.frame_id = "/base_link"
+        pc.header.frame_id = coordinates[0]
         pc.header.stamp = rospy.Time.now()
-        pc.position.x = coordinates[0]
-        pc.position.y = coordinates[1]
-        pc.position.z = coordinates[2]
+        pc.position = position
         g.motion_plan_request.goal_constraints.position_constraints.append(pc)
         oc = OrientationConstraint()
-        r = 0.0
-        p = coordinates[3] if len(coordinates) == 4 else self.pitch
-        y = 0.0
-        (qx, qy, qz, qw) = tf.transformations.quaternion_from_euler(r, p, y)
-        oc.header.frame_id = "/base_link"
+        oc.header.frame_id = coordinates[0]
         oc.header.stamp = rospy.Time.now()
-        oc.orientation.x = qx
-        oc.orientation.y = qy
-        oc.orientation.z = qz
-        oc.orientation.w = qw
+        oc.orientation = orientation
         g.motion_plan_request.goal_constraints.orientation_constraints.append(oc)
         self.move_arm_cart_server.send_goal(g)
         rospy.loginfo('Sent move arm goal, waiting for result...')
@@ -76,6 +83,17 @@ class Arm(object):
     def _move_to_pose(self, pose):
         self.script_server.move('arm', pose, mode=self.planning_mode,
                                 blocking=self.blocking)
+
+    def _construct_target_pose(self, xyzrpy):
+        xyz = np.array(xyzrpy[:3] + [1])
+        q = tf.transformations.quaternion_from_euler(*xyzrpy[3:])
+        rotation = tf.transformations.quaternion_matrix(q)
+        transformed = rotation.dot(self.ARM_LINK_5_TO_GRIPPER_OFFSET) + xyz
+        position = Point(*transformed[0:3])
+        orientation = Quaternion(*q)
+        rospy.loginfo('Requested position: %s' % (str(xyz)))
+        rospy.loginfo('Computed position: %s' % (str(position)))
+        return position, orientation
 
     def gripper(self, state):
         self.script_server.move('gripper', state)
