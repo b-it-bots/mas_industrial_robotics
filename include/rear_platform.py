fdbe2f56e2016ab63e89507e6d08dba3ada567f4
Sergey Alexandrov <alexandrov88@gmail.com>
2013-04-24 15:45:27 +0200

    Extended RearPlatform class.

--- include/rear_platform.py
+++ include/rear_platform.py
@@ -2,6 +2,10 @@ class RearPlatformFullError(Exception):
     pass
 
 
+class RearPlatformEmptyError(Exception):
+    pass
+
+
 class RearPlatform(object):
 
     """
@@ -11,13 +15,32 @@ class RearPlatform(object):
     def __init__(self):
         self.locations = {'left': None, 'middle': None, 'right': None}
 
+    @property
+    def at(self, location):
+        assert location in self.locations.keys()
+        return self.locations[location]
+
+    @property
+    def empty(self):
+        return not any(self.locations.values())
+
     def get_free_location(self):
         for k in self.locations.keys():
             if self.locations[k] is None:
                 return k
-        raise Exception('Rear platform is full')
+        raise RearPlatformFullError('Rear platform is full')
 
-    def store_object(self, location, object_name='unknown'):
-        assert location in self.locations.keys()
-        assert self.locations[location] == None
+    def get_occupied_location(self):
+        for k in self.locations.keys():
+            if self.locations[k] is not None:
+                return k
+        raise RearPlatformEmptyError('Rear platform is empty')
+
+    def store_object(self, location=None, object_name='unknown'):
+        location = location or self.get_free_location()
         self.locations[location] = object_name
+
+    def retrieve_object(self, location=None):
+        location = location or self.get_occupied_location()
+        obj, self.locations[location] = self.locations[location], None
+        return obj
