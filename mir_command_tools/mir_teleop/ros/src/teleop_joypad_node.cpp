cffa29b21fc17d3ec586cc08032c34c3455c6ebc
Frederik Hegger <frederik.hegger@h-brs.de>
2013-09-12 12:08:48 +0200

    add teleop package

---
+++ mir_command_tools/mir_teleop/ros/src/teleop_joypad_node.cpp
@@ -0,0 +1,23 @@
+/*
+ * teleop_joypad_node.cpp
+ *
+ *  Created on: Dec 15, 2011
+ *      Author: Frederik Hegger
+ */
+
+#include "mir_teleop/teleop_joypad.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "teleop_joypad");
+	ros::NodeHandle nh("~");
+
+	TeleOpJoypad* teleop = new TeleOpJoypad(nh);
+
+	ros::Rate loop_rate(50);
+
+	ros::spin();
+
+	delete teleop;
+}
+
