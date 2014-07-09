8fe214d1e9bb3e62f19d9e50f520452dcbd57429
Frederik Hegger <frederik.hegger@h-brs.de>
2014-07-09 17:55:02 +0200

    remove unused find_drawer state

--- mir_scenarios/mir_states/ros/src/mir_states/common/perception_states.py
+++ mir_scenarios/mir_states/ros/src/mir_states/common/perception_states.py
@@ -9,35 +9,6 @@ import mcr_perception_msgs.msg
 import mcr_perception_msgs.srv
 import mir_controller_msgs.srv
 
-class find_drawer(smach.State):
-
-    def __init__(self):
-        smach.State.__init__(
-            self,
-            outcomes=['found_drawer', 'no_drawer_found', 'srv_call_failed'], output_keys=["drawer_pose_list"])
-        
-        self.drawer_finder_srv_name = '/hbrs_perception/detect_marker'
-        self.drawer_finder_srv = rospy.ServiceProxy(self.drawer_finder_srv_name, hbrs_srvs.srv.GetObjects)
-        
-    def execute(self, userdata): 
-        
-        try:
-            rospy.wait_for_service(self.drawer_finder_srv_name, 15)
-            resp = self.drawer_finder_srv()
-        except Exception, e:
-            rospy.logerr("could not execute service <<%s>>: %e", self.drawer_finder_srv_name, e)
-            return 'srv_call_failed'
-            
-        if (len(resp.objects) <= 0):
-            rospy.logerr('found no drawer')
-            return 'no_drawer_found'
-        
-        rospy.loginfo('found {0} drawers'.format(len(resp.objects)))
-        
-        userdata.drawer_pose_list = resp.objects
-        
-        return 'found_drawer'
-
 
 class find_objects(smach.State):
 
