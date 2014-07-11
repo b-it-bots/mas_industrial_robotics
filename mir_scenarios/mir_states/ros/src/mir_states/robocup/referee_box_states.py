e9700bbdb169946d57b03d9dc602105cb5fea104
Frederik Hegger <frederik.hegger@h-brs.de>
2014-07-11 17:43:42 +0200

    add parsing for the CBT

--- mir_scenarios/mir_states/ros/src/mir_states/robocup/referee_box_states.py
+++ mir_scenarios/mir_states/ros/src/mir_states/robocup/referee_box_states.py
@@ -45,7 +45,8 @@ class get_task(smach.State):
                        'BMT': 'BMT<S3,S3,S2,line(M20_100,F20_20_G,F20_20_B),S2>',
                        'BTT': 'BTT<initialsituation(<S5,(R20,M30,S40_40_B)><S2,(S40_40_G,M20,R20)><S3,(F20_20_B,M20_100,F20_20_G)>);goalsituation(<C1,line(M20_100,M30,M20)><S4,line(F20_20_G,R20,R20)><S1,line(S40_40_B,S40_40_G,F20_20_B)>)>',
                        #'BTT': 'BTT<initialsituation(<S4,(S40_40_G)>);goalsituation(<S5,line(S40_40_G)>)>',
-                       'PPT': 'PPT<S2,(M20_100,M20,F20_20_B),S1>'}
+                       'PPT': 'PPT<S2,(M20_100,M20,F20_20_B),S1>',
+                       'CBT': 'CBT<C1>'}
 
     def __init__(self):
         smach.State.__init__(self,
@@ -84,6 +85,8 @@ class get_task(smach.State):
                 userdata.ppt_platform_location = ppt_description[1]
             elif(userdata.test == "CTT"):
                 userdata.task_list = get_competitive_transportation_task(task_spec[0])
+            elif(userdata.test == "CBT"):
+                userdata.task_list = task_spec[0]
             else:
                 return 'wrong_task_format'
 
