e7fea663f58968bc733cc93051397c518b8f73f2
Santosh Thoduka <santosh.thoduka@gmail.com>
2015-06-30 15:44:56 +0200

    [ppt ssm] add states to look left and right for cavities and state to wiggle arm after placing in cavity

--- mir_scenarios/mir_states/ros/src/mir_states/robocup/precision_placement_test_sub_state_machines.py
+++ mir_scenarios/mir_states/ros/src/mir_states/robocup/precision_placement_test_sub_state_machines.py
@@ -50,8 +50,11 @@ class sub_sm_place_in_holes(smach.StateMachine):
 
             # Could to this while doing adjustment.
             smach.StateMachine.add('SELECT_OBJECTS_TO_PLACE', ppts.select_objects_to_place(),
-                transitions={'objects_selected': 'LOOK_AT_WORKSPACE',
+                transitions={'objects_selected': 'CLEAR_CAVITIES',
                              'no_more_obj_for_this_workspace': 'no_object_for_ppt_platform'})
+
+            smach.StateMachine.add('CLEAR_CAVITIES', ppts.clear_cavities(),
+                transitions={'succeeded': 'LOOK_AT_WORKSPACE'})
 
             smach.StateMachine.add('LOOK_AT_WORKSPACE', gms.move_arm('out_of_view'),
                 transitions={'succeeded': 'FIND_CAVITIES',
@@ -59,6 +62,26 @@ class sub_sm_place_in_holes(smach.StateMachine):
 
             # TODO: IF NOT ALL HAVE BEEN FOUND ? What then?
             smach.StateMachine.add('FIND_CAVITIES', gps.find_cavities(frame_id='/odom'),
+                transitions={'succeeded': 'LOOK_LEFT',
+                             'not_all_cavities_found': 'LOOK_LEFT',
+                             'timeout': 'LOOK_LEFT'})
+
+            smach.StateMachine.add('LOOK_LEFT', gms.move_arm('look_at_workspace_left'),
+                transitions={'succeeded': 'FIND_CAVITIES_LEFT',
+                             'failed': 'LOOK_LEFT'})
+
+            # TODO: IF NOT ALL HAVE BEEN FOUND ? What then?
+            smach.StateMachine.add('FIND_CAVITIES_LEFT', gps.find_cavities(frame_id='/odom'),
+                transitions={'succeeded': 'LOOK_RIGHT',
+                             'not_all_cavities_found': 'LOOK_RIGHT',
+                             'timeout': 'LOOK_RIGHT'})
+
+            smach.StateMachine.add('LOOK_RIGHT', gms.move_arm('look_at_workspace_right'),
+                transitions={'succeeded': 'FIND_CAVITIES_RIGHT',
+                             'failed': 'LOOK_RIGHT'})
+
+            # TODO: IF NOT ALL HAVE BEEN FOUND ? What then?
+            smach.StateMachine.add('FIND_CAVITIES_RIGHT', gps.find_cavities(frame_id='/odom'),
                 transitions={'succeeded': 'SELECT_OBJECT_TO_PLACE',
                              'not_all_cavities_found': 'failed',
                              'timeout': 'failed'})
@@ -77,13 +100,14 @@ class sub_sm_place_in_holes(smach.StateMachine):
                 transitions={'succeeded': 'COMPUTE_ARM_BASE_SHIFT_TO_OBJECT',
                              'failed': 'MOVE_ARM_TO_PREGRASP'})
 
-            smach.StateMachine.add('COMPUTE_ARM_BASE_SHIFT_TO_OBJECT', btts.compute_arm_base_shift_to_object('/base_link', '/gripper_tip_link'),
+            smach.StateMachine.add('COMPUTE_ARM_BASE_SHIFT_TO_OBJECT', btts.compute_arm_base_shift_to_object('/base_link', '/gripper_tip_link', 0.0, -0.02),
                 transitions={'succeeded': 'MOVE_BASE_RELATIVE',
                              'tf_error': 'COMPUTE_ARM_BASE_SHIFT_TO_OBJECT'},
                 remapping={'object_pose': 'cavity_pose'})
 
             smach.StateMachine.add('MOVE_BASE_RELATIVE', gns.move_base_relative(),
                 transitions={'succeeded': 'GRASP_OBJECT_FOR_HOLE_FROM_PLTF',
+                             'unreachable': 'GRASP_OBJECT_FOR_HOLE_FROM_PLTF',
                              'timeout': 'MOVE_BASE_RELATIVE'})
 
 #            smach.StateMachine.add('MOVE_ARM_TO', gms.move_arm(),
@@ -107,14 +131,23 @@ class sub_sm_place_in_holes(smach.StateMachine):
 #            smach.StateMachine.add('SELECT_ARM_POSITION', ppts.select_arm_position(),
 #                transitions={'arm_pose_selected': 'MOVE_ARM'})
 
+
             smach.StateMachine.add('MOVE_ARM', gms.move_arm(),
                 transitions={'succeeded': 'RELEASE_OBJECT',
                              'failed': 'MOVE_ARM'})
 
             # TODO check if this, and next could cause infinite loop?
             smach.StateMachine.add('RELEASE_OBJECT', gms.linear_motion(operation='release'),
-                transitions={'succeeded':'MOVE_ARM_TO_INTERMEDIATE_POSE',
+                transitions={'succeeded':'WIGGLE_ARM_LEFT',
                              'failed':'RELEASE_OBJECT'})
+
+            smach.StateMachine.add('WIGGLE_ARM_LEFT', btts.ppt_wiggle_arm(wiggle_offset=-0.12),
+                transitions={'succeeded':'WIGGLE_ARM_RIGHT',
+                             'failed':'WIGGLE_ARM_RIGHT'})
+
+            smach.StateMachine.add('WIGGLE_ARM_RIGHT', btts.ppt_wiggle_arm(wiggle_offset=0.24),
+                transitions={'succeeded':'MOVE_ARM_TO_INTERMEDIATE_POSE',
+                             'failed':'MOVE_ARM_TO_INTERMEDIATE_POSE'})
 
             smach.StateMachine.add('MOVE_ARM_TO_INTERMEDIATE_POSE', gms.move_arm('out_of_view'),
                 transitions={'succeeded': 'SELECT_OBJECT_TO_PLACE',
