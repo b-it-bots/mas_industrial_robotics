d1e7cd8d25522feb04078d5c5dcbbbc4f1c70975
Santosh Thoduka <santosh.thoduka@gmail.com>
2015-09-09 12:23:47 +0200

    [basic states] add states for looping visual servoing tries and setting visual servoing result

--- mir_scenarios/mir_states/ros/src/mir_states/common/basic_states.py
+++ mir_scenarios/mir_states/ros/src/mir_states/common/basic_states.py
@@ -29,6 +29,49 @@ class init_robot(smach.State):
         
         return 'succeeded'
 
+class loop_for_vs(smach.State):
+    '''
+    This state will return 'loop' MAX-1 times.
+    On the MAX execute, 'continue' is returned.
+    '''
+    def __init__(self, MAX, sleep_time=0.0):
+        smach.State.__init__(self, outcomes=['loop', 'continue' ],
+                                   input_keys=['prev_vs_result'],
+                                   output_keys=['prev_vs_result'])
+        self.max_loop_count = MAX
+        self.loop_count = 0
+        self.sleep_time = sleep_time
+
+    def execute(self, userdata):
+        print("[loop_for_vs]: {0}".format(userdata.prev_vs_result))
+        if userdata.prev_vs_result == 'success':
+            self.loop_count = 0
+        if self.loop_count < self.max_loop_count:
+            rospy.sleep(self.sleep_time)
+            rospy.loginfo('run number: %d' % self.loop_count)
+            self.loop_count = self.loop_count + 1
+            return 'loop'
+        else:
+            self.loop_count = 0
+            return 'continue'
+
+class set_vs_status(smach.State):
+    def __init__(self, status):
+        smach.State.__init__(self, outcomes=['success'],
+                                   input_keys=['prev_vs_result'],
+                                   output_keys=['prev_vs_result'])
+        self.status = status
+
+    def execute(self, userdata):
+        print("status: {0}".format(self.status))
+        if self.status:
+            userdata.prev_vs_result = 'success'
+        else:
+            userdata.prev_vs_result = 'fail'
+        print("[set_vs_status]: {0}".format(userdata.prev_vs_result))
+
+        return 'success'
+
 class loop_for(smach.State):
     '''
     This state will return 'loop' MAX-1 times.
