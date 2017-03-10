3be41b872b8ed67f56986e8c8b4a2f4a6d9d50a7
Santosh Thoduka <santosh.thoduka@gmail.com>
2017-03-10 16:35:07 +0100

    [scenarios] moveit client state, bnt, cbt state machines

--- mir_scenarios/mir_states/ros/src/mir_states/common/manipulation_states.py
+++ mir_scenarios/mir_states/ros/src/mir_states/common/manipulation_states.py
@@ -7,17 +7,10 @@ import math
 import tf
 
 import geometry_msgs.msg
-import moveit_commander
-import moveit_msgs.msg
 import std_msgs.msg
+import geometry_msgs.msg
+import brics_actuator.msg
 
-arm_command = moveit_commander.MoveGroupCommander('arm_1')
-arm_command.set_goal_position_tolerance(0.01)
-arm_command.set_goal_orientation_tolerance(0.01)
-arm_command.set_goal_joint_tolerance(0.005)
-
-
-gripper_command = moveit_commander.MoveGroupCommander('arm_1_gripper')
 
 from tf.transformations import euler_from_quaternion
 import std_srvs.srv
@@ -29,8 +22,7 @@ class Bunch:
     def __init__(self, **kwds):
          self.__dict__.update(kwds)
 
-
-class move_arm(smach.State):
+class MoveitClient:
 
     """
     Move arm to a target. target may be fixed at construction time or set
@@ -46,89 +38,138 @@ class move_arm(smach.State):
         If it has 5 items, then it is arm configuration in join space.
     """
 
-    def __init__(self, target=None, blocking=True, tolerance=None):
-        smach.State.__init__(self,
-                             outcomes=['succeeded', 'failed'],
-                             input_keys=['move_arm_to'])
+    def __init__(self, moveit_group=None, target=None, timeout=10.0, joint_names=None):
+
+        TOPIC_EVENT_IN = 'moveit_client/event_in'
+        TOPIC_EVENT_OUT = 'moveit_client/event_out'
+        TOPIC_TARGET_STRING_POSE = 'moveit_client/target_string_pose'
+        TOPIC_TARGET_POSE = 'moveit_client/target_pose'
+        TOPIC_TARGET_CONFIGURATION = 'moveit_client/target_configuration'
+
         self.move_arm_to = target
-        self.blocking = blocking
-        self.tolerance = tolerance
-        
+
+        # Eventin publisher
+        self.pub_event = rospy.Publisher(moveit_group+TOPIC_EVENT_IN, std_msgs.msg.String, queue_size=1)
+
+        # Target configuration publishers
+        self.pub_target_config_name = rospy.Publisher(moveit_group+TOPIC_TARGET_STRING_POSE, std_msgs.msg.String, queue_size=1)
+        self.pub_target_pose = rospy.Publisher(moveit_group+TOPIC_TARGET_POSE, geometry_msgs.msg.PoseStamped, queue_size=1)
+        self.pub_target_config = rospy.Publisher(moveit_group+TOPIC_TARGET_CONFIGURATION, brics_actuator.msg.JointPositions, queue_size=1)
+
+        # Eventout subscriber
+        self.sub_event = rospy.Subscriber(moveit_group+TOPIC_EVENT_OUT, std_msgs.msg.String, self.event_cb)
+
+        self.client_event = None
+        self.timeout = timeout
+        self.joint_names = joint_names
+        self.moveit_group = moveit_group
+
+    def event_cb(self, msg):
+        self.client_event = msg.data
+
     def execute(self, userdata):
         target = self.move_arm_to or userdata.move_arm_to
 
-        if type(target) is str:         # target is a string specifing a joint position
-            rospy.loginfo('MOVING ARM TO: ' + str(target))
-            try:
-                arm_command.set_named_target(target)
-            except Exception as e:
-                rospy.logerr('unable to set target position: %s' % (str(e)))
-                return 'failed'
+        # do it twice because it probably fails the first time
+        for i in xrange(2):
+            self.client_event = None
+            self.pub_event.publish("e_start")
 
-        elif type(target) is list:      # target is a list ...
-            if len(target) == 7:        # ... of 7 items: Cartesian pose (x, y, z, r, p, y, frame_id)
-                try:
+            if type(target) is str:         # target is a string specifing a joint position
+                rospy.loginfo('MOVING ARM TO: ' + str(target))
+                self.pub_target_config_name.publish(target)
+
+            elif type(target) is list:      # target is a list ...
+                if len(target) == 7:        # ... of 7 items: Cartesian pose (x, y, z, r, p, y, frame_id)
                     pose = geometry_msgs.msg.PoseStamped()
                     pose.header.frame_id = target[6]
                     pose.pose.position.x = float(target[0])
                     pose.pose.position.y = float(target[1])
                     pose.pose.position.z = float(target[2])
-                    
+
                     q = tf.transformations.quaternion_from_euler(target[3], target[4], target[5])
                     pose.pose.orientation.x = q[0]
                     pose.pose.orientation.y = q[1]
                     pose.pose.orientation.z = q[2]
                     pose.pose.orientation.w = q[3]
 
-                    arm_command.set_pose_target(pose)
-                except Exception as e:
-                    rospy.logerr('unable to set target position: %s' % (str(e)))
-                    return 'failed'
-            elif len(target) == 5:      # ... of 5 items: Joint space configuration
-                try:
-                    arm_command.set_joint_value_target(target)               
-                except Exception as e:
-                    rospy.logerr('unable to set target position: %s' % (str(e)))
+                    self.pub_target_pose.publish(pose)
+                elif len(target) == 5:      # ... of 5 items: Joint space configuration
+                    self.pub_target_config.publish(self.list_to_brics_joint_positions(target))
+                else:
+                    rospy.logerr("target list is malformed")
                     return 'failed'
             else:
-                rospy.logerr("target list is malformed")
+                rospy.logerr("no valid target specified. Target should be a string (name of a joint position) or a list of 7 items (Cartesian Pose + frame id)")
                 return 'failed'
-        else:
-            rospy.logerr("no valid target specified. Target should be a string (name of a joint position) or a list of 7 items (Cartesian Pose + frame id)")
-            return 'failed'
 
-        # plan and execute arm movement        
-        error_code = arm_command.go(wait=self.blocking)
-        error_code = arm_command.go(wait=self.blocking)
-        if error_code == moveit_msgs.msg.MoveItErrorCodes.SUCCESS:
-            return 'succeeded'
-        else:
-            rospy.logerr("Arm movement failed with error code: %d", error_code)
-            return 'failed'
+            timeout = rospy.Duration.from_sec(self.timeout)
+            rate = rospy.Rate(10)
+            start_time = rospy.Time.now()
+            redo = False
+            while True:
+              if (rospy.Time.now() - start_time) > timeout:
+                  rospy.loginfo("Moveit " + self.moveit_group + "client node response timeout.")
+                  break
+              if self.client_event:
+                  if self.client_event == 'e_success':
+                      return 'succeeded'
+                  else:
+                      return 'failed'
+              rate.sleep()
 
+        return 'failed'
+
+    def list_to_brics_joint_positions(self, joint_positions):
+        brics_joint_positions =  brics_actuator.msg.JointPositions()
+        for idx, joint_position in enumerate(joint_positions):
+            joint_value = brics_actuator.msg.JointValue()
+            joint_value.timeStamp = rospy.Time.now()
+            joint_value.joint_uri = self.joint_names[idx]
+            joint_value.unit = 'rad'
+            joint_value.value = joint_position
+
+            brics_joint_positions.positions.append(joint_value)
+
+        return brics_joint_positions
+
+class move_arm(smach.State):
+
+    def __init__(self, target=None, blocking=True, tolerance=None, timeout=10.0):
+        smach.State.__init__(self,
+                             outcomes=['succeeded', 'failed'],
+                             input_keys=['move_arm_to'])
+
+        joint_names = ['arm_joint_1', 'arm_joint_2', 'arm_joint_3', 'arm_joint_4', 'arm_joint_5']
+        self.arm_moveit_client  = MoveitClient('/arm_', target, timeout, joint_names)
+
+    def execute(self, userdata):
+
+        return self.arm_moveit_client.execute(userdata)
 
 class control_gripper(smach.State):
 
-    """
-    Open or close gripper (depending on the value passed to the constructor).
-    """
-
-    def __init__(self, action):
+    def __init__(self, target=None, blocking=True, tolerance=None, timeout=10.0):
         smach.State.__init__(self, outcomes=['succeeded'])
-        self.action = action
+
+        joint_names = ['gripper_motor_right_joint', 'gripper_motor_left_joint']
+        self.gripper_moveit_client  = MoveitClient('/gripper_', target, timeout, joint_names)
 
     def execute(self, userdata):
-        gripper_command.set_named_target(self.action)
-        gripper_command.go()
-        
+
+        while True:
+
+           status = self.gripper_moveit_client.execute(userdata)
+           if status == 'succeeded':
+               break
+
         return 'succeeded'
-       
 
 class linear_motion(smach.State):
 
     """
     Should be called after visual servoing has aligned the gripper with the object.
-    Should probably be renamed in the future, or seperated into linear motion and grasping/releasing. 
+    Should probably be renamed in the future, or seperated into linear motion and grasping/releasing.
     """
 
     def __init__(self, operation='grasp', offset_x=0.0):
@@ -138,13 +179,13 @@ class linear_motion(smach.State):
         self.event_out = rospy.Publisher('/arm_relative_motion_controller/event_in', std_msgs.msg.String)
         rospy.Subscriber('/arm_relative_motion_controller/event_out', std_msgs.msg.String, self.event_cb)
         self.offset_x = offset_x
-        
+
 
     def execute(self, userdata):
         rospy.set_param('/arm_relative_motion_controller/relative_distance_x', self.offset_x)
         self.result = None
 
-        if self.operation == 'grasp': 
+        if self.operation == 'grasp':
             gripper_command.set_named_target('open')
             gripper_command.go(wait=True)
         elif self.operation == 'release':
@@ -157,7 +198,7 @@ class linear_motion(smach.State):
 
         if self.result.data != 'e_success':
             return 'failed'
-        
+
         if self.operation == 'grasp':
             gripper_command.set_named_target('close')
             gripper_command.go(wait=True)
@@ -169,36 +210,6 @@ class linear_motion(smach.State):
 
     def event_cb(self, msg):
         self.result = msg
-   
-class place_object_in_configuration(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, 
-            outcomes=['succeeded', 'no_more_cfg_poses'],
-            input_keys=['obj_goal_configuration_poses'],
-            output_keys=['obj_goal_configuration_poses'])
-                
-    def execute(self, userdata):
-        
-        if len(userdata.obj_goal_configuration_poses) == 0:
-            rospy.logerr("no more configuration poses")
-            return 'no_more_cfg_poses'
-        
-        cfg_goal_pose = userdata.obj_goal_configuration_poses.pop()
-        print "goal pose taken: ",cfg_goal_pose
-        print "rest poses: ", userdata.obj_goal_configuration_poses
-        
-        arm_command.set_named_target(cfg_goal_pose)
-        arm_command.go()
-        arm_command.go()
-        
-        gripper_command.set_named_target("open")
-        gripper_command.go()
-        
-        #arm_command.set_named_target("platform_intermediate")
-        #arm_command.go()
-                
-        return 'succeeded'
-
 
 class compute_pregrasp_pose(smach.State):
     """
@@ -284,7 +295,7 @@ class update_robot_planning_scene(smach.State):
 
         self.pub_object_id.publish(userdata.object.database_id)
         self.pub_event.publish("e_" + self.action)
-        
+
         return 'succeeded'
 
 class select_arm_pose(smach.State):
@@ -299,7 +310,7 @@ class select_arm_pose(smach.State):
                  input_keys=['next_arm_pose_index'],
                              output_keys=['move_arm_to','next_arm_pose_index'])
         self.pose_name_list = pose_name_list
-    
+
     def execute(self, userdata):
         if type(userdata.next_arm_pose_index) is not int:
              userdata.next_arm_pose_index = 0
