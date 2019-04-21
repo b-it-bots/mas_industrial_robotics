765e284ad99e95ae358f96eebdfd0d9498943e61
Mohammad Wasil <mwasil@outlook.co.id>
2019-04-21 11:41:58 +0200

    add fisher vector feature extraction (+color)

--- common/src/pcl_classifiers/features.py
+++ common/src/pcl_classifiers/features.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import sklearn.decomposition
+import pcl_classifiers.utils as utils
 
 
 def pca_compress(pointcloud, n_components=3):
@@ -61,7 +62,6 @@ def normalize_pointcloud(pointcloud):
         normalized_pointcloud = R.dot(pointcloud_xyz.T).T
 
     return normalized_pointcloud
-
 
 def calculate_centre_of_gravity_offset(pointcloud):
     """
@@ -245,7 +245,7 @@ def calculate_slices_description(pointcloud, number_of_slices):
     slice_size = pointcloud_xyz.shape[0]
     slices = np.zeros([number_of_slices, slice_size, pointcloud_xyz.shape[1]])
 
-    for k in xrange(number_of_slices):
+    for k in range(number_of_slices):
         # define the points in the slice as all points between left and right edge
         # "multiply" is used to combine two conditions
         slice = pointcloud_xyz[np.where(
@@ -254,17 +254,16 @@ def calculate_slices_description(pointcloud, number_of_slices):
         if slice.shape[0] < 1:
             continue
         slices[k, 0:slice.shape[0], :] = slice
-
+        #print ("slice shape: {}, k: {}, slices shape: {}".format(slice.shape, k, slices.shape))
         # calculate features for the slice
         radius, inlier_error, outlier_error, radial_density = fit_circle(slice, 1, 2)
-        slices[k, slice.shape[0] + 1, :] = np.array([radius, outlier_error/inlier_error, radial_density])
+        slices[k, slice.shape[0], :] = np.array([radius, outlier_error/inlier_error, radial_density])
 
         # move to the next slice
         left_edge += step
         right_edge += step
 
     return slices
-
 
 def calculate_feature_vector(pointcloud, enable_color=False):
     """
@@ -297,37 +296,163 @@ def calculate_feature_vector(pointcloud, enable_color=False):
     """
     pointcloud = normalize_pointcloud(pointcloud)
 
-    plane_radius, plane_inlier_error, plane_outlier_error, plane_radial_density = fit_circle(pointcloud, 0, 1)
-    plane_error_rate = plane_outlier_error / plane_inlier_error
-
+    # BBox feature: 3
     xyz = calculate_bounding_box(pointcloud)
 
+    # CoG: 1
     centre_of_gravity_offset = calculate_centre_of_gravity_offset(pointcloud)
-
-    slices = calculate_slices_description(pointcloud, 8)
-
-    if enable_color:
-        colour_mean = calculate_mean_colour(pointcloud)
-        colour_median = calculate_median_colour(pointcloud)
 
     features = np.array([])
     features = np.append(features, xyz)
     features = np.append(features, centre_of_gravity_offset)
 
     if enable_color:
+        colour_mean = calculate_mean_colour(pointcloud)
+        colour_median = calculate_median_colour(pointcloud)
         features = np.append(features, colour_mean)
         features = np.append(features, colour_median)
+
+    # Fit circle for whole points
+    # plane radius: 1, plane radial density: 1, plane_error_rate: 1
+    plane_radius, plane_inlier_error, plane_outlier_error, plane_radial_density = fit_circle(pointcloud, 0, 1)
+    plane_error_rate = plane_outlier_error / plane_inlier_error
     features = np.append(features, plane_radius)
     features = np.append(features, plane_error_rate)
     features = np.append(features, plane_radial_density)
 
-    for k in xrange(slices.shape[0]):
+    slices = calculate_slices_description(pointcloud, 8)
+    for k in range(slices.shape[0]):
         slice = slices[k, :, :]
-        slice = slice[np.where(np.sum(np.abs(slice),
-                      axis=1) > 0)]
+        slice = slice[np.where(np.sum(np.abs(slice),axis=1) > 0)]
         if slice.shape[0] < 1:
             features = np.append(features, np.zeros([3]))
         else:
             features = np.append(features, slice[-1, :])
 
     return features
+
+def calculate_slice_features(pointcloud, number_of_slices):
+    axis_features = []
+    pointcloud_xyz = pointcloud[:, 0:3]
+    #print (pointcloud.shape)
+    for i in range(3):
+        min_x = np.min(pointcloud_xyz[:, i])
+        max_x = np.max(pointcloud_xyz[:, i])
+
+        length_of_x_axis = max_x - min_x
+        step = length_of_x_axis / number_of_slices
+
+        # slices are bounded by left and right edge
+        # start from minimum x (left edge)
+        left_edge = min_x
+        right_edge = left_edge + step
+
+        slice_size = pointcloud_xyz.shape[0]
+        
+        slices = np.zeros([number_of_slices, slice_size, pointcloud_xyz.shape[1]])
+
+        if i == 0:
+            dim1 = 0
+            dim2 = 1
+        elif i == 1:
+            dim1 = 0
+            dim2 = 2
+        elif i == 2:
+            dim1 =1
+            dim2 = 2
+
+        for k in range(number_of_slices):
+            # define the points in the slice as all points between left and right edge
+            # "multiply" is used to combine two conditions
+            slice = pointcloud_xyz[np.where(
+                np.multiply(pointcloud_xyz[:, i] >= left_edge,
+                            pointcloud_xyz[:, i] < right_edge))]
+            if slice.shape[0] < 1:
+                continue
+            slices[k, 0:slice.shape[0], :] = slice
+            # calculate features for the slice
+            radius, inlier_error, outlier_error, radial_density = fit_circle(slice, dim1, dim2)
+            slices[k, slice.shape[0], :] = np.array([radius, outlier_error/inlier_error, radial_density])
+            # move to the next slice
+            left_edge += step
+            right_edge += step
+                    
+        axis_features.append(slices)
+    
+    return axis_features
+
+# Compute Multi-Axis Radial Density Distribution and Fisher Vector
+# Combine the feature
+def calculate_maxrdd_fv_features(pointcloud, gmm, feature_extraction, mean_circle=False):
+    pointcloud = normalize_pointcloud(pointcloud)
+
+    features = []
+    slice_features = calculate_slice_features(pointcloud, 8)
+    slice_features = np.asarray(slice_features)
+    #print (slice_features.shape)
+    for slices in slice_features:
+        s_feature = []
+        for k in range(slices.shape[0]):
+            slice = slices[k, :, :]
+            slice = slice[np.where(np.sum(np.abs(slice),axis=1) > 0)]
+            if slice.shape[0] < 1:
+                s_feature.append(np.zeros([3]))
+            else:
+                s_feature.append(slice[-1, :])
+        s_feature = np.asarray(s_feature).flatten()  
+        features.extend(s_feature)
+    
+    # Use mean circle feature such as color, BBox, outlier error
+    if mean_circle:
+        plane_radius, plane_inlier_error, plane_outlier_error, plane_radial_density = fit_circle(pointcloud, 0, 1)
+        plane_error_rate = np.divide(plane_outlier_error, plane_inlier_error)
+        features.append(plane_radius)
+        features.append(plane_error_rate)
+        features.append(plane_radial_density)
+
+        bbox = calculate_bounding_box(pointcloud)
+        centre_of_gravity_offset = calculate_centre_of_gravity_offset(pointcloud)
+        
+        # Bounding box does improve classifier
+        features.append(bbox[0])
+        features.append(bbox[1])
+        features.append(bbox[2])
+        features.append(centre_of_gravity_offset)
+
+        # Add color feature
+        colour_mean = calculate_mean_colour(pointcloud)
+        colour_median = calculate_median_colour(pointcloud)
+        features.append(colour_mean[0])
+        features.append(colour_mean[1])
+        features.append(colour_mean[2])
+        features.append(colour_median[0])
+        features.append(colour_median[1])
+        features.append(colour_median[2])
+
+    features = np.asarray(features)
+    maxrdd = features.flatten()
+    
+    # Use color feature
+    pcl_color = pointcloud[:,3:6]
+    pcl_color = normalize_pointcloud(pcl_color)
+    pcl_color = np.expand_dims(pcl_color, 0)
+    fv_color = utils.get_3DmFV(pcl_color, gmm.weights_, gmm.means_, np.sqrt(gmm.covariances_))
+    
+    # Point feature
+    pointcloud = np.expand_dims(pointcloud[:,0:3], 0)
+    fv = utils.get_3DmFV(pointcloud, gmm.weights_, gmm.means_, np.sqrt(gmm.covariances_))
+    fv = np.concatenate([fv_color, fv])
+    fv = fv.flatten()
+
+    if feature_extraction == 'msc_fv_1' or feature_extraction == 'msc_fv_2' or \
+        feature_extraction == 'msc_fv_3' or feature_extraction == 'msc_fv_4' or \
+        feature_extraction == 'msc_fv_5':
+        maxrdd_fv_features = np.hstack((maxrdd, fv))
+        maxrdd_fv_features = np.asarray(maxrdd_fv_features)
+
+    elif feature_extraction == 'fv_1' or feature_extraction == 'fv_2' or \
+        feature_extraction == 'fv_3' or feature_extraction == 'fv_4' or \
+        feature_extraction == 'fv_5':
+        maxrdd_fv_features = fv
+        
+    return maxrdd_fv_features
