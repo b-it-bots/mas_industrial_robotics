6a8b07deb9872ceb46b06c2db1ddfacdbbb6c584
Dharmin <dharmingeo@gmail.com>
2019-10-18 21:26:38 +0200

    Updated action states based on new yb actions

--- mir_scenarios/mir_states/ros/src/mir_states/common/action_states.py
+++ mir_scenarios/mir_states/ros/src/mir_states/common/action_states.py
@@ -1,113 +1,96 @@
 #!/usr/bin/python
 
 import rospy
-import roslib
-import actionlib
+from actionlib import SimpleActionClient
 import smach
-import smach_ros
 
-import sys
-import collections
-
-from mir_yb_action_msgs.msg import MoveBaseSafeAction, MoveBaseSafeGoal
-from mir_yb_action_msgs.msg import PerceiveLocationAction, PerceiveLocationGoal
-from mir_yb_action_msgs.msg import PickObjectWBCAction, PickObjectWBCGoal
-from mir_yb_action_msgs.msg import StageObjectAction, StageObjectGoal
-from mir_yb_action_msgs.msg import PlaceObjectAction, PlaceObjectGoal
-from mir_yb_action_msgs.msg import UnStageObjectAction, UnStageObjectGoal
-from mir_yb_action_msgs.msg import PerceiveLocationAction, PerceiveLocationGoal
-from mir_yb_action_msgs.msg import InsertObjectAction, InsertObjectGoal
+from actionlib_msgs.msg import GoalStatus
+from mir_yb_action_msgs.msg import GenericExecuteAction, GenericExecuteGoal
+from diagnostic_msgs.msg import KeyValue
 
 class place_object(smach.State):
     def __init__(self, platform_name):
-        smach.State.__init__(self,
-                              outcomes=['success', 'failed'])
-        self.place_client = actionlib.SimpleActionClient('place_object_server', PlaceObjectAction)
+        smach.State.__init__(self, outcomes=['success', 'failed'])
+        self.place_client = SimpleActionClient('place_object_server', GenericExecuteAction)
         self.place_client.wait_for_server()
-        self.goal = PlaceObjectGoal()
-        self.goal.object = ''
-        self.goal.location = platform_name
+        self.goal = GenericExecuteGoal()
+        self.goal.parameters.append(KeyValue(key='location', value=str(platform_name).upper()))
 
     def execute(self, userdata):
         self.place_client.send_goal(self.goal)
         self.place_client.wait_for_result(rospy.Duration.from_sec(15.0))
-        result = self.place_client.get_result()
-        if(result):
+        state = self.place_client.get_state()
+        if state == GoalStatus.SUCCEEDED:
             return 'success'
         else:
             return 'failed'
 
 class pick_object(smach.State):
     def __init__(self):
-        smach.State.__init__(self,
-                              outcomes=['success', 'failed'])
-        self.pick_client = actionlib.SimpleActionClient('wbc_pick_object_server', PickObjectWBCAction) 
+        smach.State.__init__(self, outcomes=['success', 'failed'])
+        self.pick_client = SimpleActionClient('wbc_pick_object_server', GenericExecuteAction) 
         self.pick_client.wait_for_server()
-        self.goal = PickObjectWBCGoal()
-        self.goal.object = "any"
+        self.goal = GenericExecuteGoal()
+        self.goal.parameters.append(KeyValue(key='object', value='any'))
 
     def execute(self, userdata):
         self.pick_client.send_goal(self.goal)
         self.pick_client.wait_for_result(rospy.Duration.from_sec(30.0))
-        result = self.pick_client.get_result()
-        if(result):
+        state = self.place_client.get_state()
+        if state == GoalStatus.SUCCEEDED:
             return 'success'
         else:
             return 'failed'
 
 class perceive_location(smach.State):
     def __init__(self):
-        smach.State.__init__(self,
-                              outcomes=['success', 'failed'])
-        self.perceive_location_client = actionlib.SimpleActionClient('perceive_location_server', PerceiveLocationAction)
+        smach.State.__init__(self, outcomes=['success', 'failed'])
+        self.perceive_location_client = SimpleActionClient('perceive_location_server', GenericExecuteAction)
         self.perceive_location_client.wait_for_server()
-        self.goal = PerceiveLocationGoal()
-        self.goal.location = ""
+        self.goal = GenericExecuteGoal()
 
     def execute(self, userdata):
         self.perceive_location_client.send_goal(self.goal)
         self.perceive_location_client.wait_for_result(rospy.Duration.from_sec(30.0))
-        result = self.perceive_location_client.get_result()
-        if(result):
+        state = self.place_client.get_state()
+        if state == GoalStatus.SUCCEEDED:
             return 'success'
         else:
             return 'failed'
 
 class move_base(smach.State):
     def __init__(self, destination_location):
-        smach.State.__init__(self,
-                              outcomes=['success', 'failed'])
-        self.move_base_client =  actionlib.SimpleActionClient('move_base_safe_server', MoveBaseSafeAction)
+        smach.State.__init__(self, outcomes=['success', 'failed'])
+        self.move_base_client = SimpleActionClient('move_base_safe_server', GenericExecuteAction)
         self.move_base_client.wait_for_server()
-        self.goal = MoveBaseSafeGoal()
-        self.goal.arm_safe_position = 'barrier_tape'
-        self.goal.source_location = ''
-        self.goal.destination_location = destination_location
+        self.goal = GenericExecuteGoal()
+        self.goal.parameters.append(KeyValue(key='arm_safe_position', value='barrier_tape'))
+        self.goal.parameters.append(KeyValue(key='destination_location',
+                                             value=str(destination_location).upper()))
 
     def execute(self, userdata):
         self.move_base_client.send_goal(self.goal)
         self.move_base_client.wait_for_result(rospy.Duration.from_sec(int(15.0)))
-        result = self.move_base_client.get_result()
-        if(result):
+        state = self.place_client.get_state()
+        if state == GoalStatus.SUCCEEDED:
             return 'success'
         else:
             return 'failed'
 
 class insert_object(smach.State):
     def __init__(self, robot_platform, container):
-        smach.State.__init__(self,
-                              outcomes=['success', 'failed'])
-        self.insert_object_client =  actionlib.SimpleActionClient('insert_object_server', InsertObjectAction)
+        smach.State.__init__(self, outcomes=['success', 'failed'])
+        self.insert_object_client = SimpleActionClient('insert_object_server', GenericExecuteAction)
         self.insert_object_client.wait_for_server()
-        self.goal = InsertObjectGoal()
-        self.goal.robot_platform = robot_platform
-        self.goal.hole = container
+        self.goal = GenericExecuteGoal()
+        self.goal.parameters.append(KeyValue(key='robot_platform', value=str(robot_platform).upper()))
+        self.goal.parameters.append(KeyValue(key='hole', value=str(container).upper()))
 
     def execute(self, userdata):
         self.insert_object_client.send_goal(self.goal)
         self.insert_object_client.wait_for_result(rospy.Duration.from_sec(int(30.0)))
-        result = self.insert_object_client.get_result()
-        if(result):
+        state = self.place_client.get_state()
+        if state == GoalStatus.SUCCEEDED:
             return 'success'
         else:
             return 'failed'
