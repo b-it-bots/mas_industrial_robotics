92aaf040db94de52d5b14ab55ce32aa60213ff89
Dharmin <dharmingeo@gmail.com>
2019-10-30 14:38:59 +0100

    [navigation] Renamed mcr_move_base to mir_move_base

--- mir_navigation/mir_move_base/setup.py
+++ mir_navigation/mir_move_base/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['mcr_move_base_ros'],
-    package_dir={'mcr_move_base_ros': 'ros/src/mcr_move_base_ros'}
+    packages=['mir_move_base_ros'],
+    package_dir={'mir_move_base_ros': 'ros/src/mir_move_base_ros'}
 )
 
 setup(**d)
