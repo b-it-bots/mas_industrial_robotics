d7c5a4e0bdb5c6b89c12f8a52daa95931559c636
Mohammad Wasil <mwasil@outlook.co.id>
2019-12-09 18:32:49 +0100

    import modules

--- common/src/rgb_object_recognition/config/__init__.py
+++ common/src/rgb_object_recognition/config/__init__.py
@@ -1,13 +1,1 @@
-import sys
-import os 
-
-BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'src/config'))
-#print(sys.path)
-
-from kitti_model_config import kitti_model_config
-#from kitti_vgg16_config import kitti_vgg16_config
-#from kitti_res50_config import kitti_res50_config
 from kitti_squeezeDet_config import kitti_squeezeDet_config
-#from kitti_squeezeDetPlus_config import kitti_squeezeDetPlus_config
