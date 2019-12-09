a32b26a0d74e622d17e776315a468f743f0361b4
Mohammad Wasil <mwasil@outlook.co.id>
2019-12-09 18:55:12 +0100

    rename and move some files to different directory

--- common/src/rgb_object_recognition/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'src/dataset/'))
-
-from kitti import kitti
-from pascal_voc import pascal_voc
