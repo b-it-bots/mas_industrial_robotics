29cf85b6b0b97a0b954a246fdcdd084a934d42bd
Mohammad Wasil <mwasil@outlook.co.id>
2019-12-09 18:33:16 +0100

    import modules

--- common/src/rgb_object_recognition/nets/__init__.py
+++ common/src/rgb_object_recognition/nets/__init__.py
@@ -1,10 +1,2 @@
-import os, sys
-
-BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'src/nets/'))
-
+from nn_skeleton import ModelSkeleton
 from squeezeDet import SqueezeDet
-#from squeezeDetPlus import SqueezeDetPlus
-#from resnet50_convDet import ResNet50ConvDet
-#from vgg16_convDet import VGG16ConvDet
