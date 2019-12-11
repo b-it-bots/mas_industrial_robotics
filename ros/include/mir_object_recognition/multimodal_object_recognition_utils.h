43424bfb2b1d0b311c2b775fdf32f71c46e2fc38
Mohammad Wasil <mwasil@outlook.co.id>
2019-12-11 14:04:15 +0100

    * Add Doxy style documentation for each function

--- ros/include/mir_object_recognition/multimodal_object_recognition_utils.h
+++ ros/include/mir_object_recognition/multimodal_object_recognition_utils.h
@@ -39,17 +39,50 @@ class MultimodalObjectRecognitionUtils
         virtual ~MultimodalObjectRecognitionUtils();
 
     public:
-        geometry_msgs::PoseStamped estimatePose(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &xyz_input_cloud, std::string name);
+        /** \brief Estimate pose given a cluster generated from 3D proposal. Passthrough filter
+         *         is also applied to remove part of the table on the cluster. Returns estimated pose.
+         *         The orientation is calculated based on
+         *         http://www.pcl-users.org/Finding-oriented-bounding-box-of-a-cloud-td4024616.html
+         * \param[in] the generated cluster
+         * \param[in] the label of the cluster (optional), and is used to filter round objects from passthrough
+        */
+        geometry_msgs::PoseStamped estimatePose(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &xyz_input_cloud, 
+                                                std::string name = "None");
         
-        // Update object pose for axis and bolt
+        /** \brief Make adjustment for AXIS and Bolt (M20_100) pose. Adjust it so that the pose
+         *         is close to the center of mass (the tip).
+         * \param[in] object.pointcloud
+         * 
+        */
         void adjustAxisBoltPose(mas_perception_msgs::Object &object);
-        // fix container pose
-        void adjustContainerPose(mas_perception_msgs::Object &container_object, float rgb_container_height);
+
+        /** \brief Adjust container pose by finding the largest cluster to find the center
+         * \param[in] object.pointcloud
+         * \param[in] the height adjustment of the container, default 10cm
+        */
+        void adjustContainerPose(mas_perception_msgs::Object &container_object, float rgb_container_height=0.1);
+
+        /** \brief Transform pose 
+         * \param[in] source_frame id
+         * \param[in] target_frame id
+         * \param[in] Source pose stamped
+         * \param[out] Transformed pose stamped
+        */
         void transformPose(std::string &source_frame, std::string &target_frame, 
-                            geometry_msgs::PoseStamped &pose, geometry_msgs::PoseStamped &transformed_pose);
-        void saveDebugImage(const cv_bridge::CvImagePtr &cv_image_ptr, const sensor_msgs::ImageConstPtr &raw_image, std::string logdir="/tmp/");
+                           geometry_msgs::PoseStamped &pose, 
+                           geometry_msgs::PoseStamped &transformed_pose);
+
+        /** \brief Save debug image if debug_mode is enabled
+         * \param[in] image with boundix boxes of objects drawn
+         * \param[in] raw_image
+         * \param[in] logdir (default /tmp)
+        */
+        void saveDebugImage(const cv_bridge::CvImagePtr &cv_image_ptr, 
+                            const sensor_msgs::ImageConstPtr &raw_image, 
+                            std::string logdir="/tmp/");
     
     private:
+        // transform listener
         boost::shared_ptr<tf::TransformListener> tf_listener_;
 
 
