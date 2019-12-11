1c5824a3fd8a4269ba1ce403aad548f4ea8aa906
Mohammad Wasil <mwasil@outlook.co.id>
2019-12-11 14:03:14 +0100

    change function name to camelCase

--- ros/include/mir_object_recognition/pointcloud_segmentation_ros.h
+++ ros/include/mir_object_recognition/pointcloud_segmentation_ros.h
@@ -7,52 +7,28 @@
 #ifndef MIR_OBJECT_RECOGNITION_POINTCLOUD_SEGMENTATION_ROS_H
 #define MIR_OBJECT_RECOGNITION_POINTCLOUD_SEGMENTATION_ROS_H
 
+#include <string>
+
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
 #include <tf/transform_listener.h>
-#include "mcr_scene_segmentation/cloud_accumulation.h"
-#include <mcr_scene_segmentation/scene_segmentation.h>
 
+#include <mcr_scene_segmentation/cloud_accumulation.h>
+#include <mcr_scene_segmentation/scene_segmentation.h>
+#include <mcr_scene_segmentation/SceneSegmentationConfig.h>
+#include <mas_perception_libs/bounding_box.h>
 #include <mas_perception_msgs/ObjectList.h>
 
 #include <dynamic_reconfigure/server.h>
-#include <mcr_scene_segmentation/SceneSegmentationConfig.h>
-#include <string>
-
-#include <mas_perception_libs/bounding_box.h>
-
-#include <mir_object_recognition/multimodal_object_recognition_utils.h>
-
-//using mas_perception_libs::BoundingBox;
-
-/**
- * This node subscribes to pointcloud topic.
- * Inputs:
- * ~event_in:
- *      - e_start: starts subscribing to pointcloud topic
- *      - e_add_cloud_start: adds pointcloud to octree, if it is on dataset collection mode,
- *                           the node will start segmenting the pointcloud.
- *      - e_add_cloud_stop: stops adding pointcloud to octree
- *      - e_find_plane: finds the plane and publishes workspace height
- *      - e_segment: starts segmentation and publish ObjectList
- *      - e_reset: clears accumulated cloud
- *      - e_stop: stops subscribing and clears accumulated pointcloud
- * Outputs:
- * ~event_out:
- *      - e_started: started listening to new messages
- *      - e_add_cloud_started: started adding the cloud to octree
- *      - e_add_cloud_stopped: stopped adding the cloud to octree
- *      - e_done: started finding the plane or started segmenting the pointcloud
- *      - e_stopped: stopped subscribing and cleared accumulated pointcloud
- */
 
 class PointcloudSegmentationROS
 {
     public:
-        PointcloudSegmentationROS(ros::NodeHandle nh, boost::shared_ptr<tf::TransformListener> tf_listener=nullptr);
+        PointcloudSegmentationROS(ros::NodeHandle nh, 
+                                  boost::shared_ptr<tf::TransformListener> tf_listener=nullptr);
         virtual ~PointcloudSegmentationROS();
     
     private:
@@ -63,6 +39,7 @@ class PointcloudSegmentationROS
 
         boost::shared_ptr<tf::TransformListener> tf_listener_;
 
+        // Create unique pointer object of cloud_accumulation
         CloudAccumulation::UPtr cloud_accumulation_;
 
         bool add_to_octree_;
@@ -78,9 +55,9 @@ class PointcloudSegmentationROS
         void savePcd(const PointCloud::ConstPtr &cloud, std::string obj_name);
     
     public:
-        void reset_cloud_accumulation();
-        void add_cloud_accumulation(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
-        void segment_cloud(mas_perception_msgs::ObjectList &obj_list,
+        void resetCloudAccumulation();
+        void addCloudAccumulation(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud);
+        void segmentCloud(mas_perception_msgs::ObjectList &obj_list,
                             std::vector<PointCloud::Ptr> &clusters);
         void get3DBoundingBox(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud, 
                                                 const Eigen::Vector3f& normal, 
