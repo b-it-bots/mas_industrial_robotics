e695e9a3119fd86ab2f3d4c313ccb071c196e12f
Dharmin <dharmingeo@gmail.com>
2020-01-03 14:08:26 +0100

    [docs] Fixed build errors and added maxdepth in index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,3 +62,7 @@ html_theme_path = [theme_path]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Master document should be index.rst instead of contents.rst
+# https://github.com/readthedocs/readthedocs.org/issues/2569#issuecomment-485117471
+master_doc = 'index'
