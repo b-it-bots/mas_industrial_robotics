6c1bb261e0eaf7e54b85f627c5a5c9cacd070b2c
Mohammad Wasil <mwasil@outlook.co.id>
2020-01-06 17:58:58 +0100

    Fix sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme"]
+#extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,9 +54,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-theme_path = os.path.join(current_file_dir, "..", "third_party",
-                          "open3d_sphinx_theme")
-html_theme_path = [theme_path]
+theme_path = os.path.join(current_file_dir, "..", "..","third_party",
+                          "sphinx_rtd_theme")
+html_theme_path = [theme_path, ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
