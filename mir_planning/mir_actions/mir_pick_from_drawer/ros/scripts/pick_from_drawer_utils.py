1032974e9baaa93598423fbcaac0314d410b22f0
Dharmin <dharmingeo@gmail.com>
2020-01-29 13:00:45 +0100

    [drawer_pick] Added skeleton for ros pkg

---
+++ mir_planning/mir_actions/mir_pick_from_drawer/ros/scripts/pick_from_drawer_utils.py
@@ -0,0 +1,15 @@
+#! usr/bin/env python
+from __future__ import print_function
+
+import tf
+import copy
+import math
+import rospy
+from geometry_msgs.msg import PoseStamped, Quaternion
+
+class PickFromDrawerUtils(object):
+
+    """Util class to help pick_from_drawer_server"""
+
+    def __init__(self):
+        pass
