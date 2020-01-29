1dd3b36690f3bcaffcca00994d1c9c6aa5cff58e
Dharmin <dharmingeo@gmail.com>
2020-01-29 20:41:12 +0100

    [mir_actions][pick_from_drawer] Added dbc params config file and rosparam

--- mir_planning/mir_actions/mir_pick_from_drawer/ros/scripts/pick_from_drawer_utils.py
+++ mir_planning/mir_actions/mir_pick_from_drawer/ros/scripts/pick_from_drawer_utils.py
@@ -12,13 +12,19 @@ class PickFromDrawerUtils(object):
     """Util class to help pick_from_drawer_server"""
 
     def __init__(self):
-        pass
+        self._dbc_params = rospy.get_param('~dbc_params', None)
+        if self._dbc_params is None:
+            rospy.logfatal('Params not available')
 
-    def calc_pose_for_dbc_for_param(self, param_name='default'):
+    def calc_pose_for_dbc_for_param(self, pose_in, param_name='default'):
         """Calculate pose for dbc based on the param
 
+        :pose_in: geometry_msgs.PoseStamped
         :param_name: str
         :returns: geometry_msgs.PoseStamped
 
         """
-        pass
+        if param_name in self._dbc_params:
+            print(self._dbc_params[param_name])
+        output_pose = PoseStamped()
+        return output_pose
