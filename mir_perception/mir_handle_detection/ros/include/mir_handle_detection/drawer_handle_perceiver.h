9fc90de46fbd4f5f6944d28655aeee92d6a579d9
Dharmin <dharmingeo@gmail.com>
2020-01-30 23:00:55 +0100

    [mir_handle_detection] Refactored and cleaned code

---
+++ mir_perception/mir_handle_detection/ros/include/mir_handle_detection/drawer_handle_perceiver.h
@@ -0,0 +1,66 @@
+#ifndef DRAWER_HANDLE_PERCEIVER_H
+#define DRAWER_HANDLE_PERCEIVER_H
+
+#include <ros/ros.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl_ros/transforms.h>
+
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/norms.h>
+
+#include <Eigen/Eigenvalues>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PCloudT;
+class DrawerHandlePerceiver
+{
+    public:
+        DrawerHandlePerceiver();
+        virtual ~DrawerHandlePerceiver();
+
+    private:
+        ros::NodeHandle nh;
+        ros::Subscriber pc_sub;
+        ros::Subscriber event_in_sub;
+        ros::Publisher pc_pub;
+        ros::Publisher pose_pub;
+        ros::Publisher event_out_pub;
+
+        std::string output_frame;
+        bool enable_debug_pc_pub;
+        bool listening;
+        tf::TransformListener tf_listener;
+
+        pcl::PassThrough<pcl::PointXYZ> passthrough_filter_y;
+        pcl::PassThrough<pcl::PointXYZ> passthrough_filter_z;
+        pcl::VoxelGrid<pcl::PointXYZ> voxel_grid_filter;
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        pcl::ProjectInliers<pcl::PointXYZ> project_inliers;
+        pcl::ConvexHull<pcl::PointXYZ> convex_hull;
+        pcl::ExtractPolygonalPrismData<pcl::PointXYZ> extract_polygonal_prism;
+        pcl::ExtractIndices<pcl::PointXYZ> extract_indices;
+        pcl::EuclideanClusterExtraction<pcl::PointXYZ> euclidean_cluster_extraction;
+
+        void pcCallback(const sensor_msgs::PointCloud2::ConstPtr &msg);
+        void eventInCallback(const std_msgs::String::ConstPtr &msg);
+        bool transformPC(const sensor_msgs::PointCloud2::ConstPtr &msg, sensor_msgs::PointCloud2 &msg_transformed);
+        void downSamplePC(PCloudT::Ptr input, PCloudT::Ptr output, int scale);
+        void passthroughFilterPC(PCloudT::Ptr input, PCloudT::Ptr output);
+        void extractPlaneOutlier(PCloudT::Ptr input, PCloudT::Ptr dense_input, PCloudT::Ptr output);
+        bool getClosestCluster(PCloudT::Ptr input, Eigen::Vector4f &closest_centroid);
+};
+#endif
--- mir_perception/mir_handle_detection/ros/src/drawer_handle_perceiver.cpp
+++ mir_perception/mir_handle_detection/ros/src/drawer_handle_perceiver.cpp
@@ -1,162 +1,236 @@
-#include <ros/ros.h>
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include "pcl_ros/point_cloud.h"
-#include <pcl_ros/transforms.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/surface/convex_hull.h>
-#include <pcl/segmentation/extract_polygonal_prism_data.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/project_inliers.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/common/centroid.h>
-#include <pcl/common/norms.h>
-#include <Eigen/Eigenvalues>
-#include <geometry_msgs/PoseStamped.h>
-#include <std_msgs/String.h>
+#include <mir_handle_detection/drawer_handle_perceiver.h>
 
-ros::Publisher cloud_pub;
-ros::Publisher pose_pub;
-ros::Publisher event_out_pub;
-tf::TransformListener *listenerptr;
-bool publish_output_pc;
-bool listening = false;
-std::string output_pc_frame;
-float z_threshold_min;
-float z_threshold_max;
-float y_threshold_min;
-float y_threshold_max;
-float x_threshold;
-
-void cloud_cb (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& input)
+DrawerHandlePerceiver::DrawerHandlePerceiver() : nh("~")
 {
-    if (!listening)
+    nh.param<std::string>("output_frame", this->output_frame, "base_link_static");
+    nh.param<bool>("enable_debug_pc_pub", this->enable_debug_pc_pub, "true");
+
+    this->pc_sub = nh.subscribe("input_point_cloud", 1, &DrawerHandlePerceiver::pcCallback, this);
+    this->pose_pub = nh.advertise<geometry_msgs::PoseStamped>("output_pose", 1);
+    this->event_in_sub = nh.subscribe("event_in", 1, &DrawerHandlePerceiver::eventInCallback, this);
+    this->event_out_pub = nh.advertise<std_msgs::String>("event_out", 1);
+
+    if (this->enable_debug_pc_pub)
+        this->pc_pub = nh.advertise<sensor_msgs::PointCloud2>("output_point_cloud", 1);
+
+    float z_threshold_max, z_threshold_min;
+    nh.param<float>("z_threshold_min", z_threshold_min, -0.1);
+    nh.param<float>("z_threshold_max", z_threshold_max, 0.3);
+    this->passthrough_filter_z.setFilterFieldName ("z");
+    this->passthrough_filter_z.setFilterLimits (z_threshold_min, z_threshold_max);
+
+    float y_threshold_max, y_threshold_min;
+    nh.param<float>("y_threshold_min", y_threshold_min, -0.1);
+    nh.param<float>("y_threshold_max", y_threshold_max, 0.1);
+    this->passthrough_filter_y.setFilterFieldName ("y");
+    this->passthrough_filter_y.setFilterLimits (y_threshold_min, y_threshold_max);
+
+    float leaf_size_x, leaf_size_y, leaf_size_z;
+    nh.param<float>("leaf_size_x", leaf_size_x, 0.01);
+    nh.param<float>("leaf_size_y", leaf_size_y, 0.01);
+    nh.param<float>("leaf_size_z", leaf_size_z, 0.01);
+    this->voxel_grid_filter.setLeafSize (leaf_size_x, leaf_size_y, leaf_size_z);
+
+    float seg_dist_threshold;
+    nh.param<float>("seg_dist_threshold", seg_dist_threshold, 0.01);
+    this->seg.setOptimizeCoefficients(true);
+    this->seg.setModelType(pcl::SACMODEL_PLANE);
+    this->seg.setMethodType(pcl::SAC_RANSAC);
+    this->seg.setDistanceThreshold(seg_dist_threshold);
+
+    this->project_inliers.setModelType(pcl::SACMODEL_NORMAL_PARALLEL_PLANE);
+    this->project_inliers.setCopyAllData(false);
+
+    float polygon_prism_height_min, polygon_prism_height_max;
+    nh.param<float>("polygon_prism_height_min", polygon_prism_height_min, 0.01);
+    nh.param<float>("polygon_prism_height_max", polygon_prism_height_max, 0.1);
+    this->extract_polygonal_prism.setHeightLimits(polygon_prism_height_min, polygon_prism_height_max);
+
+    this->extract_indices.setNegative(false);
+
+    float cluster_tolerance;
+    int min_cluster_size, max_cluster_size;
+    nh.param<float>("cluster_tolerance", cluster_tolerance, 0.02);
+    nh.param<int>("min_cluster_size", min_cluster_size, 50);
+    nh.param<int>("max_cluster_size", max_cluster_size, 10000);
+    this->euclidean_cluster_extraction.setClusterTolerance(cluster_tolerance);
+    this->euclidean_cluster_extraction.setMinClusterSize(min_cluster_size);
+    this->euclidean_cluster_extraction.setMaxClusterSize(max_cluster_size);
+
+    this->listening = false;
+}
+
+DrawerHandlePerceiver::~DrawerHandlePerceiver()
+{
+}
+
+void DrawerHandlePerceiver::eventInCallback(const std_msgs::String::ConstPtr &msg)
+{
+    if (msg->data == "e_start")
     {
+        ROS_INFO_STREAM("starting listening");
+        this->listening = true;
+    }
+    else if (msg->data == "e_stop")
+    {
+        ROS_INFO_STREAM("stoping listening");
+        this->listening = false;
+    }
+}
+
+void DrawerHandlePerceiver::pcCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+    if (!this->listening)
         return;
-    }
 
-    //Downsample by x3
-    int scale = 3;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_downsampled(new pcl::PointCloud<pcl::PointXYZ>);
-    cloud_downsampled->width = input->width / scale;
-    cloud_downsampled->height = input->height / scale;
-    cloud_downsampled->points.resize(cloud_downsampled->width * cloud_downsampled->height);
-    for( size_t i = 0, ii = 0; i < cloud_downsampled->height; ii += scale, i++)
+    sensor_msgs::PointCloud2 msg_transformed;
+    bool success = this->transformPC(msg, msg_transformed);
+    if (!success)
+        return;
+
+    PCloudT::Ptr pc_input(new PCloudT);
+    pcl::fromROSMsg(msg_transformed, *pc_input);
+
+    PCloudT::Ptr pc_downsampled(new PCloudT);
+    this->downSamplePC(pc_input, pc_downsampled, 3);
+
+    PCloudT::Ptr pc_passthrough_filtered(new PCloudT);
+    this->passthroughFilterPC(pc_downsampled, pc_passthrough_filtered);
+
+    PCloudT::Ptr pc_filtered(new PCloudT);
+    this->voxel_grid_filter.setInputCloud(pc_passthrough_filtered);
+    this->voxel_grid_filter.filter(*pc_filtered);
+
+    PCloudT::Ptr pc_segmented(new PCloudT);
+    this->extractPlaneOutlier(pc_filtered, pc_passthrough_filtered, pc_segmented);
+
+    Eigen::Vector4f closest_centroid(0.0, 0.0, 0.0, 0.0);
+    bool cluster_success = this->getClosestCluster(pc_segmented, closest_centroid);
+    if (!cluster_success)
+        return;
+
+    geometry_msgs::PoseStamped pose_stamped;
+    pose_stamped.pose.position.x = closest_centroid[0];
+    pose_stamped.pose.position.y = closest_centroid[1];
+    pose_stamped.pose.position.z = closest_centroid[2];
+    pose_stamped.header.frame_id = this->output_frame;
+    pose_stamped.header.stamp = ros::Time::now();
+    this->pose_pub.publish(pose_stamped);
+
+    std_msgs::String event_out_msg;
+    event_out_msg.data = "e_done";
+    this->event_out_pub.publish (event_out_msg);
+    ROS_INFO("[drawer_handle_perceiver] SUCCESSFULL");
+
+    this->listening = false;
+
+    if (this->enable_debug_pc_pub)
     {
-        for( size_t j = 0, jj = 0; j < cloud_downsampled->width; jj += scale, j++)
-        {
-            cloud_downsampled->at(j, i) = input->at(jj, ii); //at(column, row)
-        }
+        /* publish debug pointcloud */
+        sensor_msgs::PointCloud2 output;
+        pcl::toROSMsg(*pc_segmented, output);
+        output.header.frame_id = this->output_frame;
+        output.header.stamp = ros::Time::now();
+        this->pc_pub.publish(output);
+        ROS_INFO("Publishing debug pointcloud");
     }
+}
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+bool DrawerHandlePerceiver::transformPC(const sensor_msgs::PointCloud2::ConstPtr &msg, sensor_msgs::PointCloud2 &msg_transformed)
+{
+    msg_transformed.header.frame_id = this->output_frame;
     try
     {
-        ros::Time input_stamp;
-        pcl_conversions::fromPCL(input->header.stamp, input_stamp);  
+        ros::Time common_time;
+        this->tf_listener.getLatestCommonTime(this->output_frame, msg->header.frame_id, common_time, NULL);
         tf::StampedTransform transform;
-        listenerptr->waitForTransform(output_pc_frame, input->header.frame_id, input_stamp, ros::Duration(1.0));
-        listenerptr->lookupTransform(output_pc_frame, input->header.frame_id, input_stamp, transform);
-        pcl_ros::transformPointCloud(*cloud_downsampled, *cloud_transformed, transform);
+        this->tf_listener.waitForTransform(this->output_frame, msg->header.frame_id,
+                                           common_time, ros::Duration(1.0));
+        this->tf_listener.lookupTransform(this->output_frame, msg->header.frame_id,
+                                          common_time, transform);
+        pcl_ros::transformPointCloud(this->output_frame, transform, *msg, msg_transformed);
+        return true;
     }
-    catch (tf::TransformException ex)
+    catch (tf::TransformException &ex)
     {
-        ROS_ERROR("Could not find transform. Retrying...");
-        /* ROS_ERROR("%s",ex.what()); */
-        return;
+        ROS_WARN("PCL transform error: %s", ex.what());
+        ros::Duration(1.0).sleep();
+        return false;
     }
+}
 
-    // Filter points in z direction
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud_transformed);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (z_threshold_min, z_threshold_max);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pass_through(new pcl::PointCloud<pcl::PointXYZ>);
-    pass.filter (*cloud_pass_through);
+void DrawerHandlePerceiver::downSamplePC(PCloudT::Ptr input, PCloudT::Ptr output, int scale)
+{
+    output->width = input->width / scale;
+    output->height = input->height / scale;
+    output->points.resize(output->width * output->height);
+    for( size_t i = 0, ii = 0; i < output->height; ii += scale, i++)
+    {
+        for( size_t j = 0, jj = 0; j < output->width; jj += scale, j++)
+        {
+            output->at(j, i) = input->at(jj, ii); //at(column, row)
+        }
+    }
+}
 
-    // Filter points in y direction
-    pass.setInputCloud (cloud_pass_through);
-    pass.setFilterFieldName ("y");
-    pass.setFilterLimits (y_threshold_min, y_threshold_max);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pass_through_2(new pcl::PointCloud<pcl::PointXYZ>);
-    pass.filter (*cloud_pass_through_2);
+void DrawerHandlePerceiver::passthroughFilterPC(PCloudT::Ptr input, PCloudT::Ptr output)
+{
+    PCloudT::Ptr pc_intermediate(new PCloudT);
+    this->passthrough_filter_z.setInputCloud(input);
+    this->passthrough_filter_z.filter(*pc_intermediate);
+    this->passthrough_filter_y.setInputCloud(pc_intermediate);
+    this->passthrough_filter_y.filter(*output);
+}
 
-    //Voxel grid filter for uniform pointcloud
-    pcl::VoxelGrid<pcl::PointXYZ> sor;
-    sor.setInputCloud (cloud_pass_through_2);
-    sor.setLeafSize (0.005f, 0.01f, 0.01f);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-    sor.filter (*cloud_filtered);
-
-    //Estimate most dominant plane coefficients
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-    pcl::SACSegmentation<pcl::PointXYZ> seg;
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.005);
-    seg.setInputCloud (cloud_filtered);
-    seg.segment (*inliers, *coefficients);
+void DrawerHandlePerceiver::extractPlaneOutlier(PCloudT::Ptr input, PCloudT::Ptr dense_input, PCloudT::Ptr output)
+{
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    this->seg.setInputCloud(input);
+    this->seg.segment(*inliers, *coefficients);
 
     //Project plane model inliers to plane
-    pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::ProjectInliers<pcl::PointXYZ> project_inliers;
-    project_inliers.setModelType(pcl::SACMODEL_NORMAL_PARALLEL_PLANE);
-    project_inliers.setInputCloud(cloud_filtered);
-    project_inliers.setModelCoefficients(coefficients);
-    project_inliers.setIndices(inliers);
-    project_inliers.setCopyAllData(false);
-    project_inliers.filter(*plane);
+    PCloudT::Ptr pc_plane(new PCloudT);
+    this->project_inliers.setInputCloud(input);
+    this->project_inliers.setModelCoefficients(coefficients);
+    this->project_inliers.setIndices(inliers);
+    this->project_inliers.filter(*pc_plane);
 
     //Compute plane convex hull
-    pcl::PointCloud<pcl::PointXYZ>::Ptr hull(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::ConvexHull<pcl::PointXYZ> convex_hull;
-    convex_hull.setInputCloud(plane);
-    convex_hull.reconstruct(*hull);
+    PCloudT::Ptr pc_hull(new PCloudT);
+    this->convex_hull.setInputCloud(pc_plane);
+    this->convex_hull.reconstruct(*pc_hull);
 
     //Extract points inside polygonal prism of plane convex hull
     pcl::PointIndices::Ptr segmented_cloud_inliers(new pcl::PointIndices);
-    pcl::ExtractPolygonalPrismData<pcl::PointXYZ> extract_polygonal_prism;
-    extract_polygonal_prism.setInputPlanarHull(hull);
-    extract_polygonal_prism.setInputCloud(cloud_pass_through_2);
-    extract_polygonal_prism.setHeightLimits(0.005, 0.1);
-    extract_polygonal_prism.segment(*segmented_cloud_inliers);
+    this->extract_polygonal_prism.setInputPlanarHull(pc_hull);
+    this->extract_polygonal_prism.setInputCloud(dense_input);
+    this->extract_polygonal_prism.segment(*segmented_cloud_inliers);
 
-    // Extract the inliers
-    pcl::ExtractIndices<pcl::PointXYZ> extract;
-    extract.setInputCloud (cloud_pass_through_2);
-    extract.setIndices (segmented_cloud_inliers);
-    extract.setNegative (false);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr segmented_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    extract.filter (*segmented_cloud);
+    // Extract the outliers
+    this->extract_indices.setInputCloud(dense_input);
+    this->extract_indices.setIndices(segmented_cloud_inliers);
+    this->extract_indices.filter(*output);
+}
 
-    /* //Find clusters of the inlier points */
+bool DrawerHandlePerceiver::getClosestCluster(PCloudT::Ptr input, Eigen::Vector4f &closest_centroid)
+{
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
     std::vector<pcl::PointIndices> clusters_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-    ec.setClusterTolerance (0.02); // 2cm
-    ec.setMinClusterSize (50);
-    ec.setMaxClusterSize (10000);
-    ec.setSearchMethod (tree);
-    ec.setInputCloud (segmented_cloud);
-    ec.extract (clusters_indices);
+    this->euclidean_cluster_extraction.setSearchMethod(tree);
+    this->euclidean_cluster_extraction.setInputCloud(input);
+    this->euclidean_cluster_extraction.extract(clusters_indices);
+
+    if (clusters_indices.size() == 0)
+        return false;
 
     float closest_dist = 100.0;
-    Eigen::Vector4f closest_centroid(0.0, 0.0, 0.0, 0.0);
     Eigen::Vector4f zero_point(0.0, 0.0, 0.0, 0.0);
     for (size_t i = 0; i < clusters_indices.size(); i++)
     {
         const pcl::PointIndices& cluster_indices = clusters_indices[i];
         Eigen::Vector4f centroid;
-        pcl::compute3DCentroid(*segmented_cloud, cluster_indices, centroid);
+        pcl::compute3DCentroid(*input, cluster_indices, centroid);
         float dist = pcl::L2_Norm(centroid, zero_point, 3);
         if (dist < closest_dist)
         {
@@ -164,71 +238,18 @@ void cloud_cb (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& input)
             closest_centroid = centroid;
         }
     }
-    /* std::cout << closest_dist << std::endl; */
-    /* std::cout << closest_centroid << std::endl; */
-
-    geometry_msgs::PoseStamped pose_stamped;
-    pose_stamped.pose.position.x = closest_centroid[0];
-    pose_stamped.pose.position.y = closest_centroid[1];
-    pose_stamped.pose.position.z = closest_centroid[2];
-    pose_stamped.header.frame_id = output_pc_frame;
-    pose_stamped.header.stamp = ros::Time::now();
-    pose_pub.publish(pose_stamped);
-
-    std_msgs::String event_out_msg;
-    event_out_msg.data = "e_done";
-    event_out_pub.publish (event_out_msg);
-    ROS_INFO("SUCCESSFULL");
-
-    sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(*segmented_cloud, output);
-    output.header.frame_id = output_pc_frame;
-    output.header.stamp = ros::Time::now();
-    cloud_pub.publish (output);
-    
-    listening = false;
+    return true;
 }
 
-void event_in_cb (const std_msgs::String::ConstPtr& msg)
+int main(int argc, char *argv[])
 {
-    if (msg->data == "e_start")
+    ros::init(argc, argv, "drawer_handle_perceiver");
+    DrawerHandlePerceiver dhperceiver;
+    ros::Rate loop_rate(10.0);
+    while (ros::ok())
     {
-        ROS_INFO_STREAM("starting listening");
-        listening = true;
+        loop_rate.sleep();
+        ros::spinOnce();
     }
-}
-
-int main (int argc, char** argv)
-{
-    // Initialize ROS
-    ros::init (argc, argv, "drawer_handle_perceiver");
-    ros::NodeHandle nh("~");
-
-    /* ros params */
-    nh.param<bool>("publish_output_pc", publish_output_pc, "true");
-    nh.param<std::string>("output_pc_frame", output_pc_frame, "base_link_static");
-    nh.param<float>("x_threshold", x_threshold, 0.05);
-    nh.param<float>("z_threshold_min", z_threshold_min, -0.1);
-    nh.param<float>("z_threshold_max", z_threshold_max, 0.3);
-    nh.param<float>("y_threshold_min", y_threshold_min, -0.1);
-    nh.param<float>("y_threshold_max", y_threshold_max, 0.1);
-
-    tf::TransformListener listener;
-    listenerptr = &listener;
-
-    /* Create a ROS subscriber for the input point cloud */
-    ros::Subscriber pc_sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZ> > ("input", 1, cloud_cb);
-    ros::Subscriber event_in_sub = nh.subscribe<std_msgs::String> ("event_in", 1, event_in_cb);
-    event_out_pub = nh.advertise<std_msgs::String> ("event_out", 1);
-
-    if (publish_output_pc)
-    {
-        // Create a ROS publisher for the output point cloud
-        cloud_pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
-    }
-
-    pose_pub = nh.advertise<geometry_msgs::PoseStamped> ("output_pose", 1);
-
-    // Spin
-    ros::spin ();
+    return 0;
 }
