749b233020bfc72062939e9792f82790c2facbf0
DharminB <dharmingeo@gmail.com>
2020-01-31 22:46:59 +0100

    [mir_actions][manipulate_drawer] Added outline of SM for open drawer

--- mir_planning/mir_actions/mir_manipulate_drawer/ros/src/mir_manipulate_drawer/manipulate_drawer_utils.py
+++ mir_planning/mir_actions/mir_manipulate_drawer/ros/src/mir_manipulate_drawer/manipulate_drawer_utils.py
@@ -7,16 +7,15 @@ import math
 import rospy
 from geometry_msgs.msg import PoseStamped, Quaternion
 
-class PickFromDrawerUtils(object):
+class ManipulateDrawerUtils(object):
 
-    """Util class to help pick_from_drawer_server"""
+    """Util class to help open and close drawers"""
 
     def __init__(self):
         self._dbc_params = rospy.get_param('~dbc_params', None)
-        self._arm_params = rospy.get_param('~arm_params', None)
         self._drawer_length = rospy.get_param('~drawer_length', 0.25)
         self._frame_id = rospy.get_param('~frame_id', 'base_link_static')
-        if self._dbc_params is None or self._arm_params is None:
+        if self._dbc_params is None:
             rospy.logfatal('Params not available')
 
     def calc_pose_for_dbc_for_param(self, pose_in, param_name='default'):
@@ -46,30 +45,3 @@ class PickFromDrawerUtils(object):
         dbc_target_pose.pose.orientation.w = 1.0
 
         return dbc_target_pose
-
-    def calc_pose_for_arm_for_param(self, pose_in, param_name='default'):
-        """Return a pose the youbot arm's end effector should go to based on `pose_in`.
-        The orientation is modified based on param_name 
-
-        :pose_in: geometry_msgs.PoseStamped
-        :param_name: str
-        :returns: geometry_msgs.PoseStamped
-
-        """
-        if param_name not in self._arm_params or pose_in is None:
-            return PoseStamped()
-
-        arm_param_dict = self._arm_params[param_name]
-        print(arm_param_dict)
-
-        arm_target_pose = copy.deepcopy(pose_in)
-        arm_target_pose.header.stamp = rospy.Time.now()
-        arm_target_pose.pose.position.x += arm_param_dict['x_offset']
-        arm_target_pose.pose.position.y += arm_param_dict['y_offset']
-        arm_target_pose.pose.position.z += arm_param_dict['z_offset']
-        quat = tf.transformations.quaternion_from_euler(arm_param_dict['roll'],
-                                                        arm_param_dict['pitch'],
-                                                        arm_param_dict['yaw'])
-        arm_target_pose.pose.orientation = Quaternion(*quat)
-
-        return arm_target_pose
