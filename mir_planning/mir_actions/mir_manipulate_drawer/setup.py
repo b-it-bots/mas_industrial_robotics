cedf523b18f7f8400f4ff924b3a1ae3207b16cbd
DharminB <dharmingeo@gmail.com>
2020-01-31 21:04:45 +0100

    [mir_actions][mir_pick_from_drawer]Added skeleton for separating pick_from_drawer into different action servers

---
+++ mir_planning/mir_actions/mir_manipulate_drawer/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+   packages=['mir_manipulate_drawer'],
+   package_dir={'mir_manipulate_drawer': 'ros/src/mir_manipulate_drawer'}
+)
+
+setup(**d)
