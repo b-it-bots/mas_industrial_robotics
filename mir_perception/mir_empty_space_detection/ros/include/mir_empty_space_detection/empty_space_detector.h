01c207f82c94b663f3ffba74b1df0b0888b78c48
Dharmin <dharmingeo@gmail.com>
2020-03-10 01:23:41 +0100

    [perception][empty_space] Implemented sampling method to find empty spaces

--- mir_perception/mir_empty_space_detection/ros/include/mir_empty_space_detection/empty_space_detector.h
+++ mir_perception/mir_empty_space_detection/ros/include/mir_empty_space_detection/empty_space_detector.h
@@ -4,10 +4,12 @@
 #include <ros/ros.h>
 
 #include <sensor_msgs/PointCloud2.h>
-#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseArray.h>
 #include <std_msgs/String.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/ModelCoefficients.h>
+#include <pcl/PointIndices.h>
+#include <pcl/filters/extract_indices.h>
 
 #include <mir_object_segmentation/scene_segmentation.h>
 #include <mir_object_segmentation/cloud_accumulation.h>
@@ -26,13 +28,16 @@ class EmptySpaceDetector
         ros::Subscriber pc_sub_;
         ros::Subscriber event_in_sub_;
         ros::Publisher pc_pub_;
-        ros::Publisher pose_pub_;
+        ros::Publisher pose_array_pub_;
         ros::Publisher event_out_pub_;
 
         std::string output_frame_;
         bool enable_debug_pc_pub_;
         bool add_to_octree_;
-        /* bool is_running; */
+        float empty_space_radius_;
+        float expected_num_of_points_;
+        float empty_space_pnt_cnt_perc_thresh_;
+        bool find_empty_spaces_;
         /* int retry_attempts_; */
         /* int num_of_retries_; */
         boost::shared_ptr<tf::TransformListener> tf_listener_;
@@ -42,11 +47,14 @@ class EmptySpaceDetector
         CloudAccumulation::UPtr cloud_accumulation_;
 
         pcl::KdTreeFLANN<PointT> kdtree_;
+        pcl::ExtractIndices<PointT> extract_indices_;
 
         void pcCallback(const sensor_msgs::PointCloud2::ConstPtr &msg);
         void eventInCallback(const std_msgs::String::ConstPtr &msg);
         void loadParams();
+        bool findEmptySpaces();
         bool findPlane(PointCloud::Ptr plane);
-        void findEmptySpacesOnPlane(const PointCloud::Ptr &plane);
+        void findEmptySpacesOnPlane(const PointCloud::Ptr &plane,
+                                    geometry_msgs::PoseArray &empty_space_poses);
 };
 #endif
