9dabfefed0fe725c1b8e03d0ba01ef78de95eab1
Mohammad Wasil <mwasil@outlook.co.id>
2020-05-03 14:31:37 +0200

    remove hardcoded checkpoint path, add center, pad and rotate point argument

--- common/src/pcl_object_recognition/cnn_based_classifiers.py
+++ common/src/pcl_object_recognition/cnn_based_classifiers.py
@@ -14,18 +14,15 @@ class CNNBasedClassifiers():
 
     """
 
-    def __init__(self, checkpoint_path, model):
-        #self.CHECKPOINT_FILE = os.path.join(checkpoint_path, 'model.ckpt')
-        log_dir = "/home/emha/ros_catkin_ws/src/b-it-bots/unmerged_packages_for_testing/perception/mir_object_recognition/common/config/DGCNN_asus"
-        self.CHECKPOINT_FILE = os.path.join(log_dir, 'model.ckpt')
+    def __init__(self, checkpoint_path, num_points=2048):
+        self.num_points = num_points
         with tf.Graph().as_default():
             K = 6
-            self.points_pl = tf.placeholder(tf.float32, [1, 2048, K])
+            self.points_pl = tf.placeholder(tf.float32, [1, self.num_points, K])
             self.is_training_pl = tf.placeholder(tf.bool)
             
             _, end_points = dgcnn.get_model(self.points_pl, num_classes=15, is_training=self.is_training_pl, bn_decay=None, K=K)
             
-            #predictions that is not one_hot_encoded.
             self.probabilities = end_points['Probabilities']
             self.predictions = tf.argmax(self.probabilities, 1)
                                 
@@ -34,19 +31,23 @@ class CNNBasedClassifiers():
             config = tf.ConfigProto()
             config.allow_soft_placement = True
             self.sess = tf.Session(config=config)
-            saver.restore(self.sess, self.CHECKPOINT_FILE)
-            
+            saver.restore(self.sess, checkpoint_path)
 
-    def infer_one_cloud(self, points, pad_pc=True, num_points=2048, normalize=True): 
-        if normalize:
-            points = pc_utils.normalize_pointcloud(points)
+    def infer_one_cloud(self, points, center=True, rotate=True, pad=True): 
+        if center:
+            points = pc_utils.center_pointcloud(points)
         
-        if pad_pc:
+        if rotate:
+            points = pc_utils.rotate_pointcloud(points)
+
+        if pad:
             points = points.tolist()
-            while (len(points) < num_points):
+            while (len(points) < self.num_points):
                 points.append([0,0,0,0,0,0])
 
         points = np.asarray(points)
+
+        points = pc_utils.scale_to_unit_sphere(points, normalize=False)
         points = np.expand_dims(points, 0)
 
         feed_dict = {self.points_pl: points,
@@ -55,4 +56,5 @@ class CNNBasedClassifiers():
         pred_val, probs = self.sess.run([self.predictions, self.probabilities],
                                          feed_dict=feed_dict)
         probs = probs[0][pred_val][0]
+
         return pred_val, probs 
