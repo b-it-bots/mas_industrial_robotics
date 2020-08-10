043cb33c690a53d7f0f061e570a466a34937c5ba
Mohammad Wasil <mwasil@outlook.co.id>
2020-08-10 11:06:35 +0200

    use new opencv

--- mir_perception/mir_perception_utils/common/src/bounding_box.cpp
+++ mir_perception/mir_perception_utils/common/src/bounding_box.cpp
@@ -1,13 +1,17 @@
 /*
  * Copyright 2016 Bonn-Rhein-Sieg University
  *
+ * Maintainer: Mohammad Wasil
  * Author: Sergey Alexandrov
  *
  */
 
-#include <opencv/cv.h>
-#include <algorithm>
 #include <limits>
+#include <algorithm>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types_c.h>
+#include <opencv2/core/core_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
 
 #include <pcl/common/transforms.h>
 
