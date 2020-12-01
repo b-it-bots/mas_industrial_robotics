9bcb56a59b168ce9b7bcadcc1ee0015ac2cd746c
Mohammad Wasil <mwasil@outlook.co.id>
2020-12-01 15:18:15 +0100

    use numpy which comes with tf 1.14

--- setup.sh
+++ setup.sh
@@ -40,8 +40,8 @@ function install_ros_dependencies {
 function install_perception_dependencies {
     fancy_print "Installing perception dependencies"
     sudo pip install --no-cache-dir --ignore-installed enum34
-    sudo pip install --no-cache-dir -U numpy==1.16.5
     sudo pip install --no-cache-dir -U tensorflow==1.14.0
+    #sudo pip install --no-cache-dir -U numpy==1.16.5
     sudo pip install --no-cache-dir -U scikit-learn easydict joblib
 }
 
