2ae794aa3f7ac5eec23c8e1c63f1ac0d5efe4714
Shubham2S <skshinde96@gmail.com>
2022-06-08 22:21:14 +0200

    Changed launch file to load parameters yaml file

--- mir_object_recognition/ros/launch/multimodal_object_recognition.launch.py
+++ mir_object_recognition/ros/launch/multimodal_object_recognition.launch.py
@@ -1,17 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    return LaunchDescription([
-        LifecycleNode(
-            package='mir_object_recognition',
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('mir_object_recognition'),
+        'config',
+        'scene_segmentation_constraints.yaml'
+        )
+        
+    node = LifecycleNode(
+            package='mir_object_recognition', 
             executable='mmor',
-            name='mmor',
             namespace='',
+            name='mmor', 
+            parameters = [config],
             output='screen',
             remappings=[
                 ('input_image_topic', '/camera/color/image_raw'),
                 ('input_cloud_topic', '/camera/depth/color/points'),
-            ]
-        ),
-    ])
+            ])
+    
+    ld.add_action(node)
+    return ld
