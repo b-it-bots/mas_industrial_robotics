450e15f5a9f77ee8ff6ecd6dd21ec2be240fe0d4
MannavaVivek <vivek.mannava@smail.inf.h-brs.de>
2022-08-19 11:32:12 +0200

    added files

--- mir_recognizer_scripts/mir_recognizer_scripts/rgb_object_recognition/yolov5/models/common.py
+++ mir_recognizer_scripts/mir_recognizer_scripts/rgb_object_recognition/yolov5/models/common.py
@@ -22,7 +22,7 @@ from PIL import Image
 from torch.cuda import amp
 
 
-from yolov5.utils.dataloaders import exif_transpose, letterbox
+from ..utils.dataloaders import exif_transpose, letterbox
 from ..utils.general import (LOGGER, check_requirements, check_suffix, check_version, colorstr, increment_path, make_divisible, non_max_suppression, scale_coords, xywh2xyxy, xyxy2xywh)
 from ..utils.plots import Annotator, colors, save_one_box
 from ..utils.torch_utils import copy_attr, time_sync
