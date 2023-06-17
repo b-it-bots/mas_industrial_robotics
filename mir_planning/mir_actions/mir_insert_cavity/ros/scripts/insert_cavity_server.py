cb9bd2b5fd85f8b2b529ac7d2b9cbb654eacf24d
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-06-17 12:49:57 +0200

    [mir_insert_cavity] updated the sm to work with top camera

--- mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
+++ mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
@@ -16,32 +16,50 @@ from mir_planning_msgs.msg import (
     GenericExecuteAction,
     GenericExecuteFeedback,
     GenericExecuteResult,
+    GenericExecuteGoal,
 )
 from moveit_msgs.msg import MoveItErrorCodes
 from smach_ros import ActionServerWrapper, IntrospectionServer
 from std_msgs.msg import Float64, String
+from diagnostic_msgs.msg import KeyValue
 
 # ===============================================================================
 
 # For wiggle arm
-# arm_command = moveit_commander.MoveGroupCommander("arm_1")
-# arm_command.set_goal_position_tolerance(0.01)
-# arm_command.set_goal_orientation_tolerance(0.01)
-# arm_command.set_goal_joint_tolerance(0.005)
+arm_command = moveit_commander.MoveGroupCommander("arm_1")
+arm_command.set_goal_position_tolerance(0.01)
+arm_command.set_goal_orientation_tolerance(0.01)
+arm_command.set_goal_joint_tolerance(0.005)
 
 # ===============================================================================
 
 
-class SelectObject(smach.State):
-    def __init__(self, topic_name):
+class SelectCavity(smach.State):
+    def __init__(self, topic_name, vertical=False):
         smach.State.__init__(
             self,
-            outcomes=["succeeded"],
+            outcomes=["succeeded", "failed"],
             input_keys=["goal"],
             output_keys=["feedback", "result"],
         )
+        self.vertical = vertical
         self.publisher = rospy.Publisher(topic_name, String, queue_size=10)
         rospy.sleep(0.1)  # time for the publisher to register in ros network
+
+    def obj_to_cavity(self, obj_name: str, vertical: bool = False):
+        matchings = {
+            "M20": ["M20_H", "M20_V"],
+            "M30": ["M30_H", "M30_V"],
+            "M20_100": ["M20_100_H", "M20_100_V"],
+            "F20_20": ["F20_20_H", "F20_20_V"],
+            "S40_40": ["S40_40_H", "S40_40_V"],
+        }
+
+        for key in matchings.keys():
+            if obj_name.startswith(key):
+                return matchings[key][vertical]
+        return None
+
 
     def execute(self, userdata):
         # Add empty result msg (because if none of the state do it, action server gives error)
@@ -50,14 +68,26 @@ class SelectObject(smach.State):
             current_state="SelectObject", text="selecting object"
         )
 
+        print("userdata: ", String(userdata.goal.parameters))
+
         obj = Utils.get_value_of(userdata.goal.parameters, "peg")
-        self.publisher.publish(String(data=obj))
+
+        if obj is None:
+            rospy.logerr("No object name provided")
+            return "failed"
+
+        # get the cavity name
+        cavity_name = self.obj_to_cavity(obj, self.vertical)
+
+        if cavity_name is None:
+            rospy.logerr("No cavity name found for object: " + obj)
+            return "failed"
+
+        self.publisher.publish(String(data=cavity_name))
         rospy.sleep(0.2)  # let the topic survive for some time
         return "succeeded"
 
-
 # ===============================================================================
-
 
 class ppt_wiggle_arm(smach.State):
     """
@@ -72,7 +102,8 @@ class ppt_wiggle_arm(smach.State):
 
         # create publisher for sending gripper position
         self.gripper_topic = "/gripper_controller/command"
-        self.cavity_pose_topic = "/mcr_perception/object_selector/output/object_pose"
+        # self.cavity_pose_topic = "/mcr_perception/object_selector/output/object_pose"
+        self.cavity_pose_topic = "/mir_perception/multimodel_object_recognition/output/rgb_object_pose_array"
 
         self.gripper = rospy.Publisher(self.gripper_topic, Float64, queue_size=10)
 
@@ -182,6 +213,44 @@ class ppt_wiggle_arm(smach.State):
         return "succeeded"
         #################
 
+# ===============================================================================
+
+class Unstage_to_place(smach.State):
+
+    def __init__(self):
+
+        smach.State.__init__(self, outcomes=["success","failed"],input_keys=["goal","heavy_objects", "platform","object"])
+        self.platform = "PLATFORM_MIDDLE"
+        self.obj = "M20"
+
+    def execute(self,userdata):
+
+        self.platform = Utils.get_value_of(userdata.goal.parameters, "platform")
+        self.obj = Utils.get_value_of(userdata.goal.parameters, "peg")
+
+        if self.obj is None:
+            rospy.logwarn('Missing parameter "object". Using default.')
+            self.obj = "light"
+        else:
+            self.obj =  "light"
+
+
+        self.unstage_client = SimpleActionClient('unstage_object_server', GenericExecuteAction)
+        self.unstage_client.wait_for_server()
+
+	    # Assigning the goal    
+
+        goal = GenericExecuteGoal()
+        goal.parameters.append(KeyValue(key="platform", value=self.platform))
+        goal.parameters.append(KeyValue(key="object", value=self.obj))
+
+        self.unstage_client.send_goal(goal)
+        self.unstage_client.wait_for_result(rospy.Duration.from_sec(25.0))
+
+        rospy.loginfo("Sending following goal to unstage object server")
+        rospy.loginfo(goal)
+
+        return "success"
 
 # ===============================================================================
 def transition_cb(*args, **kwargs):
@@ -211,33 +280,82 @@ def main():
         output_keys=["feedback", "result"],
     )
     with sm:
-        # publish object as string to mcr_perception_selectors -> cavity,
-        # this component then publishes pose in base_link reference frame
+        # set cavity model
         smach.StateMachine.add(
-            "SELECT_OBJECT",
-            SelectObject("/mcr_perception/cavity_pose_selector/object_name"),
-            transitions={"succeeded": "CHECK_IF_OBJECT_IS_AVAILABLE"},
+            "SET_PERCEPTION_PARAMS",
+            gbs.set_named_config("multimodal_object_recognition_cavity"),
+            transitions={
+                "success": "PERCEIVE_LOCATION",
+                "timeout": "OVERALL_FAILED",
+                "failure": "OVERALL_FAILED",
+            },
+        )
+
+        # start perception
+        smach.StateMachine.add(
+            "PERCEIVE_LOCATION",
+            gas.perceive_location(),
+            transitions={
+                "success": "SELECT_CAVITY",
+                "failed": "OVERALL_FAILED",
+            },
         )
 
         smach.StateMachine.add(
-            "CHECK_IF_OBJECT_IS_AVAILABLE",
+            "SELECT_CAVITY",
+            SelectCavity("/mcr_perception/object_selector/input/object_name", vertical=False),
+            transitions={
+                "succeeded": "GENERATE_OBJECT_POSE_AGAIN",
+                "failed": "GET_VERTICAL_CAVITY"
+            },
+        )
+
+        smach.StateMachine.add(
+            "GET_VERTICAL_CAVITY",
+            SelectCavity("/mcr_perception/object_selector/input/object_name", vertical=True),
+            transitions={
+                "succeeded": "GENERATE_OBJECT_POSE_AGAIN",
+                "failed": "OVERALL_FAILED"
+            },
+        )
+
+        # generates a pose of object
+        smach.StateMachine.add(
+            "GENERATE_UPDATED_OBJECT_POSE",
             gbs.send_and_wait_events_combined(
-                event_in_list=[
-                    ("/mcr_perception/cavity_pose_selector/event_in", "e_trigger",)
-                ],
-                event_out_list=[
-                    (
-                        "/mcr_perception/cavity_pose_selector/event_out",
-                        "e_success",
-                        True,
-                    )
-                ],
+                event_in_list=[("/mcr_perception/local_object_selector/event_in", "e_trigger")],
+                event_out_list=[("/mcr_perception/local_object_selector/event_out", "e_selected", True)],
                 timeout_duration=10,
             ),
             transitions={
-                "success": "SET_DBC_PARAMS",
+                "success": "UNSTAGE_FOR_PLACING",
+                "timeout": "GENERATE_OBJECT_POSE_AGAIN",
+                "failure": "GENERATE_OBJECT_POSE_AGAIN",
+            },
+        )
+
+        # generates a pose of object
+        smach.StateMachine.add(
+            "GENERATE_OBJECT_POSE_AGAIN",
+            gbs.send_and_wait_events_combined(
+                event_in_list=[("/mcr_perception/object_selector/event_in", "e_trigger")],
+                event_out_list=[("/mcr_perception/object_selector/event_out", "e_selected", True)],
+                timeout_duration=10,
+            ),
+            transitions={
+                "success": "UNSTAGE_FOR_PLACING",
                 "timeout": "OVERALL_FAILED",
                 "failure": "OVERALL_FAILED",
+            },
+        )
+
+        # unstage object to place
+        smach.StateMachine.add(
+            "UNSTAGE_FOR_PLACING",
+            Unstage_to_place(),
+            transitions={
+                "success":"SET_DBC_PARAMS", 
+                "failed":"OVERALL_FAILED",
             },
         )
 
@@ -245,101 +363,45 @@ def main():
             "SET_DBC_PARAMS",
             gbs.set_named_config("dbc_pick_object"),
             transitions={
-                "success": "MOVE_ROBOT_AND_TRY_PLACING",
+                "success": "MOVE_ROBOT_AND_TRY_INSERTING",
                 "timeout": "OVERALL_FAILED",
                 "failure": "OVERALL_FAILED",
             },
         )
 
         smach.StateMachine.add(
-            "MOVE_ROBOT_AND_TRY_PLACING",
+            "MOVE_ROBOT_AND_TRY_INSERTING",
             gbs.send_and_wait_events_combined(
-                event_in_list=[("/wbc/event_in", "e_try")],
+                event_in_list=[("/wbc/event_in", "e_start")],
                 event_out_list=[("/wbc/event_out", "e_success", True)],
                 timeout_duration=50,
             ),
             transitions={
-                "success": "MOVE_ARM_TO_MIDDLE_POSE",
-                "timeout": "MOVE_ARM_TO_MIDDLE_POSE",
-                "failure": "MOVE_ARM_TO_MIDDLE_POSE",
-            },
-        )
-
-        smach.StateMachine.add(
-            "MOVE_ARM_TO_MIDDLE_POSE",
-            gms.move_arm("pre_place"),
-            transitions={
-                "succeeded": "PERCEIVE_CAVITY",
-                "failed": "MOVE_ARM_TO_MIDDLE_POSE",
-            },
-        )
-        # perceive cavity again after moving in front of the desired cavity
-        smach.StateMachine.add(
-            "PERCEIVE_CAVITY",
-            gps.find_cavities(retries=1),
-            transitions={"cavities_found": "SELECT_OBJECT_AGAIN", "no_cavities_found": "OVERALL_FAILED",},
-        )
-
-        smach.StateMachine.add(
-            "SELECT_OBJECT_AGAIN",
-            SelectObject("/mcr_perception/cavity_pose_selector/object_name"),
-            transitions={"succeeded": "CHECK_IF_OBJECT_IS_AVAILABLE_AGAIN"},
-        )
-
-        smach.StateMachine.add(
-            "CHECK_IF_OBJECT_IS_AVAILABLE_AGAIN",
-            gbs.send_and_wait_events_combined(
-                event_in_list=[
-                    ("/mcr_perception/cavity_pose_selector/event_in", "e_trigger",)
-                ],
-                event_out_list=[
-                    (
-                        "/mcr_perception/cavity_pose_selector/event_out",
-                        "e_success",
-                        True,
-                    )
-                ],
-                timeout_duration=10,
-            ),
-            transitions={
-                "success": "UNSTAGE_OBJECT",
-                "timeout": "OVERALL_FAILED",
-                "failure": "OVERALL_FAILED",
-            },
-        )
-
-        smach.StateMachine.add(
-            "UNSTAGE_OBJECT",
-            gas.unstage_object(),
-            transitions={"success": "TRY_INSERTING", "failed": "OVERALL_FAILED",},
-        )
-
-        # execute robot motion
-        smach.StateMachine.add(
-            "TRY_INSERTING",
-            gbs.send_and_wait_events_combined(
-                event_in_list=[("/wbc/event_in", "e_start_arm_only")],
-                event_out_list=[("/wbc/event_out", "e_success", True)],
-                timeout_duration=20,
-            ),
-            transitions={
                 "success": "OPEN_GRIPPER",
-                "timeout": "STAGE_OBJECT",
-                "failure": "STAGE_OBJECT",
+                "timeout": "STOP_MOVE_ROBOT_TO_OBJECT_WITH_FAILURE",
+                "failure": "STOP_MOVE_ROBOT_TO_OBJECT_WITH_FAILURE",
             },
         )
 
         smach.StateMachine.add(
-            "STAGE_OBJECT",
-            gas.stage_object(),
-            transitions={"success": "OVERALL_FAILED", "failed": "OVERALL_FAILED",},
+            "STOP_MOVE_ROBOT_TO_OBJECT_WITH_FAILURE",
+            gbs.send_event(
+                [
+                    ("/waypoint_trajectory_generation/event_in", "e_stop"),
+                    ("/wbc/event_in", "e_stop"),
+                ]
+            ),
+            transitions={"success": "OVERALL_FAILED"},
         )
 
-        # close gripper
+        # open gripper
         smach.StateMachine.add(
             "OPEN_GRIPPER",
             gms.control_gripper("open"),
-            transitions={"succeeded": "MOVE_ARM_TO_HOLD"},
+            transitions={
+                "succeeded": "WIGGLE_ARM",
+                "timeout": "WIGGLE_ARM"
+            },
         )
 
         # wiggling the arm for precision placement
