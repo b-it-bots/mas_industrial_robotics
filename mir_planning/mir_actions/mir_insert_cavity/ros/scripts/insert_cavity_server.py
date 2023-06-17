7ee343341ed8162b1c6e29c1cfdd4357e3ea6eae
Kevin Patel <47410011+kvnptl@users.noreply.github.com>
2023-06-17 13:04:25 +0200

    [mir_planning][mir_actions] added obj_category as a dynamic configurable parameter > [mir_perception][mir_object_recognition] added in config files

--- mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
+++ mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
@@ -182,6 +182,41 @@ class ppt_wiggle_arm(smach.State):
         return "succeeded"
         #################
 
+# ===============================================================================
+
+class Unstage_to_place(smach.State):
+
+    def __init__(self):
+
+        smach.State.__init__(self, outcomes=["success","failed"],input_keys=["goal","heavy_objects", "platform","object"])
+        self.platform = "PLATFORM_MIDDLE"
+        self.obj = "M20"
+
+    def execute(self,userdata):
+
+        self.platform = Utils.get_value_of(userdata.goal.parameters, "platform")
+        self.obj = Utils.get_value_of(userdata.goal.parameters, "peg")
+
+        if self.obj is None:
+            rospy.logwarn('Missing parameter "object". Using default.')
+            self.obj = "light"
+        else:
+            self.obj =  "light"
+
+
+        self.unstage_client = SimpleActionClient('unstage_object_server', GenericExecuteAction)
+        self.unstage_client.wait_for_server()
+
+	    # Assigning the goal    
+
+        goal = GenericExecuteGoal()
+        goal.parameters.append(KeyValue(key="platform", value=self.platform))
+        goal.parameters.append(KeyValue(key="object", value=self.obj))
+
+        self.unstage_client.send_goal(goal)
+        self.unstage_client.wait_for_result(rospy.Duration.from_sec(25.0))
+
+        return "success"
 
 # ===============================================================================
 def transition_cb(*args, **kwargs):
@@ -220,22 +255,33 @@ def main():
         )
 
         smach.StateMachine.add(
-            "CHECK_IF_OBJECT_IS_AVAILABLE",
+            "SELECT_CAVITY",
+            SelectCavity("/mcr_perception/object_selector/input/object_name", vertical=False),
+            transitions={
+                "succeeded": "GENERATE_OBJECT_POSE",
+                "failed": "GET_VERTICAL_CAVITY"
+            },
+        )
+
+        smach.StateMachine.add(
+            "GET_VERTICAL_CAVITY",
+            SelectCavity("/mcr_perception/object_selector/input/object_name", vertical=True),
+            transitions={
+                "succeeded": "GENERATE_OBJECT_POSE",
+                "failed": "OVERALL_FAILED"
+            },
+        )
+
+        # generates a pose of object
+        smach.StateMachine.add(
+            "GENERATE_OBJECT_POSE",
             gbs.send_and_wait_events_combined(
-                event_in_list=[
-                    ("/mcr_perception/cavity_pose_selector/event_in", "e_trigger",)
-                ],
-                event_out_list=[
-                    (
-                        "/mcr_perception/cavity_pose_selector/event_out",
-                        "e_success",
-                        True,
-                    )
-                ],
+                event_in_list=[("/mcr_perception/object_selector/event_in", "e_trigger")],
+                event_out_list=[("/mcr_perception/object_selector/event_out", "e_selected", True)],
                 timeout_duration=10,
             ),
             transitions={
-                "success": "SET_DBC_PARAMS",
+                "success": "UNSTAGE_FOR_PLACING",
                 "timeout": "OVERALL_FAILED",
                 "failure": "OVERALL_FAILED",
             },
