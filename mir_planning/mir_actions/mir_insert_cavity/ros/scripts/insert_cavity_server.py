f19d8db33e5d1697d7bdadc793cb5ed65ccf8f3f
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-07-06 10:00:27 +0200

    [mir_actions] bug fix for object_name in insert object server

--- mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
+++ mir_planning/mir_actions/mir_insert_cavity/ros/scripts/insert_cavity_server.py
@@ -47,7 +47,7 @@ class SelectCavity(smach.State):
             "S40_40_B": ["S40_40_H", "S40_40_V"],
             "S40_40_G": ["S40_40_H", "S40_40_V"],
         }
-
+        obj_name = obj_name.split("-")[0]
         for key in matchings.keys():
             if obj_name == key:
                 return matchings[key][vertical]
